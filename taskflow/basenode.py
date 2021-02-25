aad5eff77bf6030a2e32af0ba23d2ddbea800c49
pedohorse <xapkohheh@gmail.com>
2021-02-25 23:52:06 +0100

    base node expanded to cover basic attributes

--- taskflow/basenode.py
+++ taskflow/basenode.py
@@ -1,7 +1,25 @@
 import asyncio
+from enum import Enum
+from typing import Dict
+
+
+class AttributeType(Enum):
+    INT = 0
+    BOOL = 1
+    FLOAT = 2
+    STRING = 3
 
 
 class BaseNode:
+    def attribs(self) -> Dict[str, AttributeType]:
+        return {}
+
+    def attrib_value(self, attrib_name):
+        raise NotImplementedError()
+
+    def set_attrib_value(self, attrib_name, attrib_value):
+        raise NotImplementedError()
+
     def process_task(self, task_dict):
         raise NotImplementedError()
 
