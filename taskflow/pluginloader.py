916861d48959f38d0a1c32eb040fd3ea39dbe09f
pedohorse <xapkohheh@gmail.com>
2021-03-07 01:58:57 +0100

    loading plugins from a list of paths

--- taskflow/pluginloader.py
+++ taskflow/pluginloader.py
@@ -9,21 +9,26 @@ def init():
     print('loading core plugins')
     global plugins
     plugins = {}
+    plugin_paths = []
     core_plugins_path = os.path.join(os.path.dirname(__file__), 'core_nodes')
-    for filename in os.listdir(core_plugins_path):
-        filebasename, fileext = os.path.splitext(filename)
-        if fileext != '.py':
-            continue
-        mod_spec = importlib.util.spec_from_file_location(f'taskflow.coreplugins.{filebasename}',
-                                                          os.path.join(core_plugins_path, filename))
-        mod = importlib.util.module_from_spec(mod_spec)
-        mod_spec.loader.exec_module(mod)
-        for requred_attr in ('create_node_object',):
-            if not hasattr(mod, requred_attr):
-                print(f'error loading plugin "{filebasename}". '
-                      f'required method {requred_attr} is missing.')
+    stock_plugins_path = os.path.join(os.path.dirname(__file__), 'stock_nodes')
+    plugin_paths.append(core_plugins_path)
+    plugin_paths.append(stock_plugins_path)
+    for plugin_path in plugin_paths:
+        for filename in os.listdir(plugin_path):
+            filebasename, fileext = os.path.splitext(filename)
+            if fileext != '.py':
                 continue
-        plugins[filebasename] = mod
+            mod_spec = importlib.util.spec_from_file_location(f'taskflow.coreplugins.{filebasename}',
+                                                              os.path.join(plugin_path, filename))
+            mod = importlib.util.module_from_spec(mod_spec)
+            mod_spec.loader.exec_module(mod)
+            for requred_attr in ('create_node_object',):
+                if not hasattr(mod, requred_attr):
+                    print(f'error loading plugin "{filebasename}". '
+                          f'required method {requred_attr} is missing.')
+                    continue
+            plugins[filebasename] = mod
     print('loaded plugins:\n', '\n\t'.join(plugins.keys()))
 
 
