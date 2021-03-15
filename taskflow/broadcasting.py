6bbb902ae7b5b59ee1b47a5d09bb6375aed853a3
pedohorse <xapkohheh@gmail.com>
2021-03-15 18:04:51 +0100

    rebroadcasting to 127.0.0.1 to faster deliver messages to local machine listeners

--- taskflow/broadcasting.py
+++ taskflow/broadcasting.py
@@ -12,11 +12,11 @@ Address = Tuple[str, int]
 _magic = b'=\xe2\x88\x88\xe2\xad\x95\xe2\x88\x8b='
 
 
-async def create_broadcaster(identifier, information, broad_port=9000):
+async def create_broadcaster(identifier, information, broad_port=9000, ip='0.0.0.0', broadcasts_count=None):
     loop = asyncio.get_event_loop()
     return await loop.create_datagram_endpoint(
-        lambda: BroadcastProtocol(identifier, information, ('0.0.0.0', broad_port), loop=loop),
-        family=socket.AF_INET, reuse_port=True)
+        lambda: BroadcastProtocol(identifier, information, (ip, broad_port), broadcasts_count, loop=loop),
+        family=socket.AF_INET, reuse_port=True, allow_broadcast=True)
 
 
 async def await_broadcast(identifier, broad_port=9000):
@@ -24,26 +24,33 @@ async def await_broadcast(identifier, broad_port=9000):
     protocol: BroadcastReceiveProtocol
     _, protocol = await loop.create_datagram_endpoint(
         lambda: BroadcastReceiveProtocol(identifier, loop=loop),
-        local_addr=('0.0.0.0', broad_port), reuse_port=True)
-    return await protocol.till_done()
+        local_addr=('0.0.0.0', broad_port), family=socket.AF_INET, reuse_port=True, allow_broadcast=True)
+    message = await protocol.till_done()
+
+    # rebroadcast to localhost
+    _, protocol = await create_broadcaster(identifier, message, broad_port, ip='127.0.0.1', broadcasts_count=1)  # TODO: bet with my future self - i will regret hardcoding ipv4 localhost like that
+    await protocol.till_done()
+    return message
 
 
 class BroadcastProtocol(asyncio.DatagramProtocol):
-    def __init__(self, identifier: str, information: str, target: Address, *, loop: asyncio.AbstractEventLoop = None):
+    def __init__(self, identifier: str, information: str, target: Address, count=None, *, loop: asyncio.AbstractEventLoop = None):
         self.__target = target
         self.__loop = asyncio.get_event_loop() if loop is None else loop
         self.__transport = None
         self.__identifier = identifier
         self.__interval = 10
+        self.__broadcast_count = count
         self.__information = information
+        self.__closed_future = self.__loop.create_future()
         # TODO: raise on dgram size too big
 
     def connection_made(self, transport: asyncio.transports.DatagramTransport):
         print('started broadcasting')
         self.__transport = transport
         sock = transport.get_extra_info("socket")  # type: socket.socket
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        #sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        #sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.__loop.create_task(self.broadcast())
 
     def datagram_received(self, data: Union[bytes, str], addr: Address):
@@ -56,7 +63,16 @@ class BroadcastProtocol(asyncio.DatagramProtocol):
             binfo = self.__information.encode('UTF-8')
             data: bytes = b''.join((_magic, struct.pack('>I', len(bitent)), struct.pack('>I', len(binfo)), bitent, binfo))
             self.__transport.sendto(data, self.__target)
+            if self.__broadcast_count is not None:
+                self.__broadcast_count -= 1
+                if self.__broadcast_count <= 0:
+                    break
             await asyncio.sleep(self.__interval)
+        self.__transport.close()
+        self.__closed_future.set_result(True)
+
+    async def till_done(self):
+        return await self.__closed_future
 
 
 class BroadcastReceiveProtocol(asyncio.DatagramProtocol):
@@ -71,7 +87,7 @@ class BroadcastReceiveProtocol(asyncio.DatagramProtocol):
         self.__transport = transport
         sock = transport.get_extra_info("socket")  # type: socket.socket
         #sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255)
+        #sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255)
         #sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
     def datagram_received(self, data: Union[bytes, str], addr: Address):
