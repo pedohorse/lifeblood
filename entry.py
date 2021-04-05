851ad968fc170ef7eaf4306b2a6cbc561e49646e
pedohorse <xapkohheh@gmail.com>
2021-04-05 16:40:18 +0200

    new global entry point

---
+++ entry.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import sys
+import os
+import argparse
+
+
+def main(argv):
+    parser = argparse.ArgumentParser('taskflow', description='task execution thingie')
+
+    subparsers = parser.add_subparsers(title='command', required=True, dest='command')
+    schedparser = subparsers.add_parser('scheduler', description='run main scheduler server')
+
+    workerparser = subparsers.add_parser('worker', description='run a worker')
+
+    viewparser = subparsers.add_parser('viewer', description='run default viewer')
+
+    opts = parser.parse_args(argv)
+
+    if opts.command == 'scheduler':
+        from taskflow.scheduler import main
+        main()
+    elif opts.command == 'worker':
+        from taskflow.worker import main
+        main()
+    elif opts.command == 'viewer':
+        from taskflow.viewer.launch import main
+        main()
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
