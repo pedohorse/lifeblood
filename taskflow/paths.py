36846f54b3db965bd90ba96bdc202a5b7112f3f5
pedohorse <xapkohheh@gmail.com>
2021-04-10 01:33:31 +0200

    changing config location to houdini-like

--- taskflow/paths.py
+++ taskflow/paths.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import Optional
 
 org = 'xxx'
+basename = 'taskflow'
 
 
 def config_path(config_name: str, subname: Optional[str] = None) -> Path:
@@ -10,11 +11,17 @@ def config_path(config_name: str, subname: Optional[str] = None) -> Path:
     if subname is None:
         subname = 'common'
     if sys.platform.startswith('linux'):
-        return base/'.local'/'share'/org/subname/config_name
+        return base/basename/subname/config_name
     if sys.platform.startswith('win'):
-        return base/'AppData'/'Roaming'/org/subname/config_name
+        return base/basename/subname/config_name
     elif sys.platform.startswith('darwin'):
-        return base/'Library'/'Application Support'/org/subname/config_name
+        return base/'Library'/'Preferences'/basename/subname/config_name
+    # if sys.platform.startswith('linux'):
+    #     return base/'.local'/'share'/org/subname/config_name
+    # if sys.platform.startswith('win'):
+    #     return base/'AppData'/'Roaming'/org/subname/config_name
+    # elif sys.platform.startswith('darwin'):
+    #     return base/'Library'/'Application Support'/org/subname/config_name
 
 
 def default_main_database_location() -> Path:
