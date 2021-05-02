7b8ca6bc28e400f4b6e11910cacde3feb4b8763a
pedohorse <xapkohheh@gmail.com>
2021-05-02 10:42:57 +0200

    added async deserialize

--- src/taskflow/invocationjob.py
+++ src/taskflow/invocationjob.py
@@ -186,3 +186,7 @@ class InvocationJob:
     @classmethod
     def deserialize(cls, data: bytes) -> "InvocationJob":
         return pickle.loads(data)
+
+    @classmethod
+    async def deserialize_async(cls, data: bytes) -> "InvocationJob":
+        return await asyncio.get_event_loop().run_in_executor(None, cls.deserialize, data)
