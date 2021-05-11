9634f2b9119b603f9d74bd996688c4e6508a8107
pedohorse <xapkohheh@gmail.com>
2021-05-12 00:57:16 +0200

    changed QStringListModel to a custom AbstractItemModel subclass treating task groups now as table using QSplitter instead of layout now for main widgets

--- src/taskflow/viewer/taskflow_viewer.py
+++ src/taskflow/viewer/taskflow_viewer.py
@@ -1,19 +1,67 @@
+from datetime import datetime, timezone
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
-from PySide2.QtCore import Qt, Slot, Signal, QStringListModel, QItemSelection
+from PySide2.QtCore import Qt, Slot, Signal, QAbstractItemModel, QItemSelection, QModelIndex
 from .nodeeditor import NodeEditor, QGraphicsImguiScene
 
+from typing import Dict
 
-class GroupsModel(QStringListModel):
+
+class GroupsModel(QAbstractItemModel):
+    SortRole = Qt.UserRole + 0
+
     def __init__(self, parent):
         super(GroupsModel, self).__init__(parent=parent)
+        self.__items = {}
+        self.__items_order = []
+
+    def headerData(self, section: int, orientation: Qt.Orientation, role: int = Qt.DisplayRole):
+        if role != Qt.DisplayRole:
+            return
+        if section == 0:
+            return 'name'
+        elif section == 1:
+            return 'creation time'
+
+    def rowCount(self, parent: QModelIndex = None) -> int:
+        if parent is None:
+            parent = QModelIndex()
+        if not parent.isValid():
+            return len(self.__items)
+        return 0
+
+    def columnCount(self, parent: QModelIndex = None) -> int:
+        return 2
+
+    def data(self, index: QModelIndex, role: int = Qt.DisplayRole):
+        if role != Qt.DisplayRole and role != self.SortRole:
+            return
+        if index.column() == 0:
+            return self.__items_order[index.row()]
+        elif index.column() == 1:
+            if role == Qt.DisplayRole:
+                return datetime.fromtimestamp(self.__items[self.__items_order[index.row()]].get('ctime', 0) or 0).replace(tzinfo=timezone.utc).astimezone().strftime(r'%H:%M:%S %d %b %y')
+            elif role == self.SortRole:
+                return self.__items[self.__items_order[index.row()]].get('ctime', -1)
+
+    def index(self, row: int, column: int, parent: QModelIndex = None) -> QModelIndex:
+        if parent is None:
+            parent = QModelIndex()
+        return self.createIndex(row, column)
+
+    def parent(self, index: QModelIndex):
+        # for now it's one level model
+        return QModelIndex()
 
     @Slot(list)
-    def update_groups(self, groups):
-        self.setStringList(list(groups))
+    def update_groups(self, groups: Dict[str, dict]):
+        self.beginResetModel()
+        self.__items = groups
+        self.__items_order = sorted(list(groups.keys()), key=lambda x: self.__items[x].get('ctime', -1) or -1, reverse=True)
+        self.endResetModel()
 
 
-class GroupsView(QListView):
+class GroupsView(QTreeView):
     selection_changed = Signal(set)
 
     def __init__(self, parent=None):
@@ -22,24 +70,29 @@ class GroupsView(QListView):
 
     def selectionChanged(self, selected: QItemSelection, deselected: QItemSelection) -> None:
         super(GroupsView, self).selectionChanged(selected, deselected)
-        self.selection_changed.emit(set(index.data() for index in self.selectedIndexes()))
+        self.selection_changed.emit(set(index.data() for index in self.selectedIndexes() if index.column() == 0))
 
 
 class TaskflowViewer(QMainWindow):
     def __init__(self, db_path: str, parent=None):
         super(TaskflowViewer, self).__init__(parent)
-        self.__central_widget = QWidget()
+        self.__central_widget = QSplitter()
         self.setCentralWidget(self.__central_widget)
-        self.__main_layout = QHBoxLayout(self.centralWidget())
+        #self.__main_layout = QHBoxLayout(self.centralWidget())
         self.__node_editor = NodeEditor(db_path)
         self.__group_list = GroupsView()
         self.__group_list.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
 
         self.__model_main = GroupsModel(self)
         self.__group_list.setModel(self.__model_main)
+        self.__group_list.header().setSectionResizeMode(0, QHeaderView.ResizeToContents)
+        self.__group_list.header().setStretchLastSection(True)
 
-        self.__main_layout.addWidget(self.__group_list)
-        self.__main_layout.addWidget(self.__node_editor)
+        #self.__main_layout.addWidget(self.__group_list)
+        #self.__main_layout.addWidget(self.__node_editor)
+        self.__central_widget.addWidget(self.__group_list)
+        self.__central_widget.addWidget(self.__node_editor)
+        self.__central_widget.setSizes([1, 999999])
         self.__group_list.setFocusPolicy(Qt.ClickFocus)
         self.__node_editor.setFocusPolicy(Qt.ClickFocus)
 
@@ -56,7 +109,7 @@ class TaskflowViewer(QMainWindow):
         do_select = self.__model_main.rowCount() == 0
         self.__model_main.update_groups(groups)
         if do_select and self.__model_main.rowCount() > 0:
-            self.__group_list.setCurrentIndex(self.__model_main.index(self.__model_main.rowCount()-1, 0))
+            self.__group_list.setCurrentIndex(self.__model_main.index(0, 0))
 
     def setSceneRect(self, *args, **kwargs):
         return self.__node_editor.setSceneRect(*args, **kwargs)
