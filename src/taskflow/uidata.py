6251a92a7e211d9a0a88f7682cb06267628715dc
pedohorse <xapkohheh@gmail.com>
2021-05-30 14:01:43 +0200

    WIP

--- src/taskflow/uidata.py
+++ src/taskflow/uidata.py
@@ -4,7 +4,7 @@ from copy import copy
 from .enums import NodeParameterType
 import re
 
-from typing import TYPE_CHECKING, TypedDict, Dict, Any, List, Optional, Tuple, Union
+from typing import TYPE_CHECKING, TypedDict, Dict, Any, List, Set, Optional, Tuple, Union, Iterable
 
 if TYPE_CHECKING:
     from .basenode import BaseNode
@@ -53,10 +53,339 @@ class UiData:
         return pickle.loads(data)
 
 
-if TYPE_CHECKING:
-    class Parameter(TypedDict):
-        type: NodeParameterType
-        value: Any
+# if TYPE_CHECKING:
+#     class Parameter(TypedDict):
+#         type: NodeParameterType
+#         value: Any
+class ParameterHierarchyItem:
+    def __init__(self):
+        self.__parent: Optional["ParameterHierarchyItem"] = None
+        self.__children: Set["ParameterHierarchyItem"] = set()
+
+    def parent(self) -> Optional["ParameterHierarchyItem"]:
+        return self.__parent
+
+    def set_parent(self, item: Optional["ParameterHierarchyItem"]):
+        if self.__parent == item:
+            return
+        if self.__parent is not None:
+            assert self in self.__parent.__children
+            self.__parent._child_about_to_be_removed(self)
+            self.__parent.__children.remove(self)
+        self.__parent = item
+        if self.__parent is not None:
+            self.__parent.__children.add(self)
+            self.__parent._child_added(self)
+
+    def _child_about_to_be_removed(self, child: "ParameterHierarchyItem"):
+        """
+        callback for just before a child is removed
+        :param child:
+        :return:
+        """
+        pass
+
+    def _child_added(self, child: "ParameterHierarchyItem"):
+        """
+        callback for just after child is added
+        :param child:
+        :return:
+        """
+        pass
+
+    def children(self) -> Iterable["ParameterHierarchyItem"]:
+        return frozenset(self.__children)
+
+    def _children_definition_changed(self, children: Iterable["ParameterHierarchyItem"]):
+        if self.__parent is not None:
+            self.__parent._children_definition_changed([self])
+
+    def _children_value_changed(self, children: Iterable["ParameterHierarchyItem"]):
+        if self.__parent is not None:
+            self.__parent._children_value_changed([self])
+
+
+class ParameterHierarchyLeaf(ParameterHierarchyItem):
+    def _children_definition_changed(self, children: Iterable["ParameterHierarchyItem"]):
+        return
+
+    def _children_value_changed(self, children: Iterable["ParameterHierarchyItem"]):
+        return
+
+    def _child_added(self, child: "ParameterHierarchyItem"):
+        raise RuntimeError('cannot add children to ParameterHierarchyLeaf')
+
+    def _child_about_to_be_removed(self, child: "ParameterHierarchyItem"):
+        raise RuntimeError('cannot remove children from ParameterHierarchyLeaf')
+
+
+class Parameter(ParameterHierarchyLeaf):
+    def __init__(self, param_name: str, param_label: Optional[str], param_type: NodeParameterType, param_val: Any):
+        super(Parameter, self).__init__()
+        self.__name = param_name
+        self.__label = param_label
+        self.__type = param_type
+        self.__value = None
+        self.set_value(param_val)
+        self.__menu = None
+
+        # caches
+        self.__vis_cache = None
+
+    def name(self) -> str:
+        return self.__name
+
+    def label(self) -> Optional[str]:
+        return self.__label
+
+    def type(self) -> NodeParameterType:
+        return self.__type
+
+    def value(self):
+        return self.__value
+
+    def set_value(self, value: Any):
+        if self.__type == NodeParameterType.FLOAT:
+            param_value = float(value)
+        elif self.__type == NodeParameterType.INT:
+            param_value = int(value)
+        elif self.__type == NodeParameterType.BOOL:
+            param_value = bool(value)
+        elif self.__type == NodeParameterType.STRING:
+            param_value = str(value)
+        else:
+            raise NotImplementedError()
+        self.__value = param_value
+        if self.parent() is not None:
+            self.parent()._children_value_changed([self])
+
+    def _reset_caches(self):
+        self.__vis_cache = None
+
+    def visible(self, param_name: str):
+
+        if self.__vis_cache is not None:
+            return self.__vis_cache
+        if '_vis_when' in param_dict:
+            other_param_name, op, value = param_dict['_vis_when']
+            other_param = self.__parameters.get(other_param_name, None)
+            if op == '==' and other_param['value'] != value \
+                    or op == '!=' and other_param['value'] == value \
+                    or op == '>' and other_param['value'] <= value \
+                    or op == '>=' and other_param['value'] < value \
+                    or op == '<' and other_param['value'] >= value \
+                    or op == '<=' and other_param['value'] > value:
+                param_dict['__vis_cache'] = False
+                return False
+        param_dict['__vis_cache'] = not param_dict.get('is_ui_modifier', False)
+        return True
+
+    def add_visibility_condition(self, param_name: str, condition: Union[str, Tuple[str, str, Any]]):
+        """
+        condition currently can only be a simplest
+        :param param_name:
+        :param condition:
+        :return:
+        """
+        if not self.__block_ui_callbacks:
+            raise RuntimeError('initializing NodeUi interface not inside initializing_interface_lock')
+        if param_name not in self.__parameters:
+            raise RuntimeError(f'parameter "{param_name}" does not exists on the node')
+        if isinstance(condition, str):
+            match = re.match(r'(\w+)(==|!=|>=|<=|<|>)(.*)', condition)
+            if match is None:
+                raise RuntimeError(f'bad visibility condition: {condition}')
+            oparam, op, value = tuple(match.groups())
+            if oparam not in self.__parameters:
+                raise RuntimeError('parameters in visibility condition must already be added to allow type checking')
+            otype = self.__parameters[oparam]['type']
+            if otype == NodeParameterType.INT:
+                value = int(value)
+            elif otype == NodeParameterType.BOOL:
+                value = bool(value)
+            elif otype == NodeParameterType.FLOAT:
+                value = float(value)
+            elif otype != NodeParameterType.STRING:  # for future
+                raise RuntimeError(f'cannot add visibility condition check based on this type of parameters: {otype}')
+            self.__parameters[param_name]['_vis_when'] = (oparam, op, value)
+        else:
+            self.__parameters[param_name]['_vis_when'] = condition
+        self.__reset_all_cache()
+
+    def add_menu_to_parameter(self, param_name: str, menu_items_pairs):
+        """
+        adds UI menu to parameter param_name
+        :param param_name: parameter to add menu to. Must already exist on the node
+        :param menu_items: dict of label -> value for parameter menu. type of value MUST match type of parameter param_name. type of label MUST be string
+        :return:
+        """
+        if not self.__block_ui_callbacks:
+            raise RuntimeError('initializing NodeUi interface not inside initializing_interface_lock')
+        if param_name not in self.__parameters:
+            raise RuntimeError(f'parameter "{param_name}" does not exists on the node')
+        # sanity check and regroup
+        param_type = self.__parameters[param_name]['type']
+        menu_items = {}
+        menu_order = []
+        for key, value in menu_items_pairs:
+            menu_items[key] = value
+            menu_order.append(key)
+            if not isinstance(key, str):
+                raise RuntimeError('menu label type must be string')
+            if param_type == NodeParameterType.INT and not isinstance(value, int):
+                raise RuntimeError(f'wrong menu value for int parameter "{param_name}"')
+            elif param_type == NodeParameterType.BOOL and not isinstance(value, bool):
+                raise RuntimeError(f'wrong menu value for bool parameter "{param_name}"')
+            elif param_type == NodeParameterType.FLOAT and not isinstance(value, float):
+                raise RuntimeError(f'wrong menu value for float parameter "{param_name}"')
+            elif param_type == NodeParameterType.STRING and not isinstance(value, str):
+                raise RuntimeError(f'wrong menu value for string parameter "{param_name}"')
+
+        self.__parameters[param_name]['menu_items'] = menu_items
+        self.__parameters[param_name]['_menu_items_order'] = menu_order
+        self.__ui_callback([param_name])
+
+
+class ParameterNotFound(RuntimeError):
+    pass
+
+
+class ParametersLayoutBase(ParameterHierarchyItem):
+    def __init__(self):
+        super(ParametersLayoutBase, self).__init__()
+        self.__parameters: Dict[str: Parameter] = {}  # just for quicker access
+        self.__layouts: Set[ParametersLayoutBase] = set()
+        self.__block_ui_callbacks = False
+
+    def initializing_interface_lock(self):
+        class _iiLock:
+            def __init__(self, lockable):
+                self.__nui = lockable
+
+            def __enter__(self):
+                self.__nui._ParametersLayoutBaseWithLock_block_ui_callbacks = True
+
+            def __exit__(self, exc_type, exc_val, exc_tb):
+                self.__nui._ParametersLayoutBaseWithLock_block_ui_callbacks = False
+
+        return _iiLock(self)
+
+    def _is_initialize_lock_set(self):
+        return self.__block_ui_callbacks
+
+    def add_parameter(self, new_parameter: Parameter):
+        if not self._is_initialize_lock_set():
+            raise RuntimeError('initializing interface not inside initializing_interface_lock')
+        new_parameter.set_parent(self)
+
+    def add_layout(self, new_layout: "ParametersLayoutBase"):
+        if not self._is_initialize_lock_set():
+            raise RuntimeError('initializing interface not inside initializing_interface_lock')
+        new_layout.set_parent(self)
+
+    def parameters(self, recursive=False) -> Iterable[Parameter]:
+        for child in self.children():
+            if isinstance(child, Parameter):
+                yield child
+            elif not recursive:
+                continue
+            elif isinstance(child, ParametersLayoutBase):
+                for child_param in child.parameters(recursive=recursive):
+                    yield child_param
+
+    def parameter(self, name: str) -> Parameter:
+        if name in self.__parameters:
+            return self.__parameters[name]
+        for layout in self.__layouts:
+            try:
+                return layout.parameter(name)
+            except ParameterNotFound:
+                continue
+        raise ParameterNotFound(f'parameter {name} not found in layout hierarchy')
+
+    def _child_added(self, child: "ParameterHierarchyItem"):
+        super(ParametersLayoutBase, self)._child_added(child)
+        if isinstance(child, Parameter):
+            if child.name() in (x.name() for x in self.parameters(recursive=True)):
+                raise RuntimeError('cannot add parameters with the same name to the same layout hierarchy')
+            self.__parameters[child.name()] = child
+        elif isinstance(child, ParametersLayoutBase):
+            self.__layouts.add(child)
+
+    def _child_about_to_be_removed(self, child: "ParameterHierarchyItem"):
+        if isinstance(child, Parameter):
+            del self.__parameters[child.name()]
+        elif isinstance(child, ParametersLayoutBase):
+            self.__layouts.remove(child)
+        super(ParametersLayoutBase, self)._child_about_to_be_removed(child)
+
+    def _children_definition_changed(self, children: Iterable["ParameterHierarchyItem"]):
+        """
+        for now we don't keep any parameter dependencies, so change to a definition
+        will cause all parameters to drop their caches
+        :param children:
+        :return:
+        """
+        if self.parent() is None:  # if we are the root
+            for parameter in self.parameters(recursive=True):
+                parameter._reset_caches()
+        super(ParametersLayoutBase, self)._children_definition_changed(children)
+
+    def _children_value_changed(self, children: Iterable["ParameterHierarchyItem"]):
+        """
+        for now we don't keep any parameter dependencies, so change to a value
+        will cause all parameters to drop their caches
+        :param children:
+        :return:
+        """
+        if self.parent() is None:  # if we are the root
+            for parameter in self.parameters(recursive=True):
+                parameter._reset_caches()
+        super(ParametersLayoutBase, self)._children_value_changed(children)
+
+
+class OrderedParametersLayout(ParametersLayoutBase):
+    def __init__(self):
+        super(OrderedParametersLayout, self).__init__()
+        self.__parameter_order: List[ParameterHierarchyItem] = []
+
+    def _child_added(self, child: "ParameterHierarchyItem"):
+        super(OrderedParametersLayout, self)._child_added(child)
+        self.__parameter_order.append(child)
+        
+    def _child_about_to_be_removed(self, child: "ParameterHierarchyItem"):
+        self.__parameter_order.remove(child)
+        super(OrderedParametersLayout, self)._child_about_to_be_removed(child)
+
+    def parameters(self, recursive=False):
+        """
+        unlike base method, we need to return parameters in order
+        :param recursive:
+        :return:
+        """
+        for child in self.__parameter_order:
+            if isinstance(child, Parameter):
+                yield child
+            elif not recursive:
+                continue
+            elif isinstance(child, ParametersLayoutBase):
+                for child_param in child.parameters(recursive=recursive):
+                    yield child_param
+
+
+class VerticalParametersLayout(OrderedParametersLayout):
+    """
+    simple vertical parameter layout.
+    """
+    pass
+
+
+class OneLineParametersLayout(OrderedParametersLayout):
+    """
+    horizontal parameter layout.
+    unlike vertical, this one has to keep track of portions of line it's parameters are taking
+    """
+    pass
 
 
 class NodeUi:
