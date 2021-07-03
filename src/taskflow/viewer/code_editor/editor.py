9030f8f713f93e713e9a8669080d3ad9c8e3b2b5
pedohorse <xapkohheh@gmail.com>
2021-07-03 14:48:25 +0200

    buttons function. syntax highlighter added

--- src/taskflow/viewer/code_editor/editor.py
+++ src/taskflow/viewer/code_editor/editor.py
@@ -1,9 +1,37 @@
+import re
+from enum import Enum
 from PySide2.QtWidgets import *
-from PySide2.QtGui import QFont
+from PySide2.QtGui import QFont, QSyntaxHighlighter, QTextCharFormat, QColor
+from PySide2.QtCore import Slot, Signal, Qt, QSize
+
+from typing import List, Tuple, Pattern
+
+
+class PythonSyntaxHighlighter(QSyntaxHighlighter):
+    def __init__(self, doc):
+        super(PythonSyntaxHighlighter, self).__init__(doc)
+
+        kw_format = QTextCharFormat()
+        kw_format.setForeground(QColor('#CC7832'))
+        kw_format.setFontWeight(QFont.Bold)
+        self.__highlights: List[Tuple[Pattern[str], QTextCharFormat]] = []
+        for keyword in ('False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield'):
+            self.__highlights.append((re.compile(rf'\b{keyword}\b'), kw_format))
+
+    def highlightBlock(self, text:str) -> None:
+        for rule, format in self.__highlights:
+            for m in rule.finditer(text):
+                self.setFormat(m.start(), m.end()-m.start(), format)
 
 
 class StringParameterEditor(QWidget):
-    def __init__(self, parent=None):
+    edit_done = Signal(str)
+
+    class SyntaxHighlight(Enum):
+        NO_HIGHLIGHT = 0
+        PYTHON = 1
+
+    def __init__(self, syntax_highlight: SyntaxHighlight = SyntaxHighlight.NO_HIGHLIGHT, parent=None):
         super(StringParameterEditor, self).__init__(parent)
         self.__main_layout = QVBoxLayout(self)
         self.__textarea = QTextEdit()
@@ -22,12 +50,35 @@ class StringParameterEditor(QWidget):
         self.__main_layout.addWidget(self.__textarea)
         self.__main_layout.addLayout(self.__bottom_layout)
 
+        if syntax_highlight == StringParameterEditor.SyntaxHighlight.PYTHON:
+            self.__syntax_highlighter = PythonSyntaxHighlighter(self.__textarea.document())
+
+        # connec
+        self.__cancel_button.clicked.connect(self.close)
+        self.__ok_button.clicked.connect(self._done_editing)
+
+    @Slot()
+    def _done_editing(self):
+        self.close()
+        self.edit_done.emit(self.text())
+
+    def sizeHint(self):
+        return QSize(640, 480)
+
+    def text(self):
+        return self.__textarea.toPlainText()
+
+    def set_text(self, text: str):
+        self.__textarea.setPlainText(text)
+
 
 def _test():
     import sys
     qapp = QApplication(sys.argv)
 
-    wgt = StringParameterEditor()
+    wgt = StringParameterEditor(StringParameterEditor.SyntaxHighlight.PYTHON)
+    wgt.set_text('line1\nline2\nline3 and more')
+    wgt.edit_done.connect(lambda s: print(f'edit done, result: {s}'))
     wgt.show()
     return qapp.exec_()
 
