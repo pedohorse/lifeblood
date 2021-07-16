d29cd5d8deec6d79b12f9e5f110a10e5a6cefe89
pedohorse <xapkohheh@gmail.com>
2021-07-17 01:03:40 +0200

    better constructor, also now holds description

--- src/taskflow/net_classes.py
+++ src/taskflow/net_classes.py
@@ -1,8 +1,11 @@
-from typing import Iterable
+from typing import TYPE_CHECKING, Type
+if TYPE_CHECKING:
+    from basenode import BaseNode
 
 
 class NodeTypeMetadata:
-    def __init__(self, type_name: str, label: str, tags: Iterable[str]):
-        self.type_name = type_name
-        self.label = label
-        self.tags = set(tags)
+    def __init__(self, node_type: Type["BaseNode"]):
+        self.type_name = node_type.type_name()
+        self.label = node_type.label()
+        self.tags = set(node_type.tags())
+        self.description = node_type.description()
