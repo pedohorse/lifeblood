bd52eec18fab0f7636ccb73aebcad8c7b8f52ef9
pedohorse <xapkohheh@gmail.com>
2021-07-18 00:31:02 +0200

    now plugin loader saves module file hash to check updates to the file later

--- src/taskflow/pluginloader.py
+++ src/taskflow/pluginloader.py
@@ -1,9 +1,15 @@
 import os
+import sys
+import hashlib
 import importlib.util
 
 from . import logging
 
+from typing import List, Tuple
+
+
 plugins = {}
+__plugin_file_hashes = {}
 
 logger = logging.getLogger('plugin_loader')
 
@@ -12,17 +18,18 @@ def init():
     logger.info('loading core plugins')
     global plugins
     plugins = {}
-    plugin_paths = []
+    plugin_paths: List[Tuple[str, str]] = []  # list of tuples of path to dir, plugin category
     core_plugins_path = os.path.join(os.path.dirname(__file__), 'core_nodes')
     stock_plugins_path = os.path.join(os.path.dirname(__file__), 'stock_nodes')
-    plugin_paths.append(core_plugins_path)
-    plugin_paths.append(stock_plugins_path)
-    for plugin_path in plugin_paths:
+    plugin_paths.append((core_plugins_path, 'core'))
+    plugin_paths.append((stock_plugins_path, 'stock'))
+    for plugin_path, plugin_category in plugin_paths:
         for filename in os.listdir(plugin_path):
             filebasename, fileext = os.path.splitext(filename)
             if fileext != '.py':
                 continue
-            mod_spec = importlib.util.spec_from_file_location(f'taskflow.coreplugins.{filebasename}',  # TODO: here all plugins load as coreplugins
+            modpath = f'taskflow.nodeplugins.{plugin_category}.{filebasename}'
+            mod_spec = importlib.util.spec_from_file_location(modpath,
                                                               os.path.join(plugin_path, filename))
             try:
                 mod = importlib.util.module_from_spec(mod_spec)
@@ -35,7 +42,16 @@ def init():
                                  f'required method {requred_attr} is missing.')
                     continue
             plugins[mod.node_class().type_name()] = mod
+            hasher = hashlib.md5()
+            with open(os.path.join(plugin_path, filename), 'rb') as f:
+                hasher.update(f.read())
+            __plugin_file_hashes[mod.node_class().type_name()] = hasher.hexdigest()
+            sys.modules[modpath] = mod
     logger.info('loaded plugins:\n' + '\n\t'.join(plugins.keys()))
+
+
+def plugin_hash(plugin_name) -> str:
+    return __plugin_file_hashes[plugin_name]
 
 
 def create_node(plugin_name: str, name, scheduler_parent, node_id):
