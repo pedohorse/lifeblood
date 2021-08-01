444bc2305102575a8e60b1e9195285eeeef8c86a
pedohorse <xapkohheh@gmail.com>
2021-08-01 16:31:13 +0200

    taking r g b args separately instead of a tuple

--- src/taskflow/node_visualization_classes.py
+++ src/taskflow/node_visualization_classes.py
@@ -6,8 +6,8 @@ class NodeColorScheme:
     def __init__(self):
         self.__main_color = (0, 0, 0)
 
-    def set_main_color(self, color: Tuple[float, float, float]):
-        self.__main_color = color
+    def set_main_color(self, r: float, g: float, b: float):
+        self.__main_color = (r, g, b)
 
     def main_color(self):
         return self.__main_color
