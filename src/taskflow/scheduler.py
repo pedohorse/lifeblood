4eb3442ccf76105968c5e58f9d64aaee55a141e4
pedohorse <xapkohheh@gmail.com>
2021-08-16 01:07:48 +0200

    starting worker pool with minimum idle set

--- src/taskflow/scheduler.py
+++ src/taskflow/scheduler.py
@@ -76,7 +76,7 @@ class Scheduler:
             self.__broadcasting_server = None
             self.__broadcasting_server_coro = None
 
-        self.__worker_pool = WorkerPool(WorkerType.SCHEDULER_HELPER)
+        self.__worker_pool = WorkerPool(WorkerType.SCHEDULER_HELPER, minimal_idle_to_ensure=1)
 
         self.__ping_interval = 1
         self.__processing_interval = 2
