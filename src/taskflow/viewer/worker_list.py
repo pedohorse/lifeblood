31e6f1f3f3914782ad2b802bb542d6cb008d521b
pedohorse <xapkohheh@gmail.com>
2021-09-15 01:01:45 +0200

    implemented minimal worker view

--- src/taskflow/viewer/worker_list.py
+++ src/taskflow/viewer/worker_list.py
@@ -1,22 +1,48 @@
+from datetime import datetime
 from ..uidata import UiData
+from ..enums import WorkerType
 from .connection_worker import SchedulerConnectionWorker
 
-from PySide2.QtWidgets import QWidget, QListView
-from PySide2.QtCore import Slot, Qt, QAbstractListModel, QModelIndex
+from PySide2.QtWidgets import QWidget, QTableView, QHBoxLayout, QHeaderView
+from PySide2.QtCore import Slot, Qt, QAbstractTableModel, QModelIndex
 
 from typing import Optional, Dict, List
 
+
 class WorkerListWidget(QWidget):
-    def __init__(self, parent=None):
-        super(WorkerListWidget, self).__init__(parent)
+    def __init__(self, worker: SchedulerConnectionWorker, parent=None):
+        super(WorkerListWidget, self).__init__(parent, Qt.Tool)
+        self.__worker_list = QTableView()
+        self.__worker_model = WorkerModel(worker, self)
+        self.__worker_list.setModel(self.__worker_model)
+        self.__worker_list.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+
+        layout = QHBoxLayout(self)
+        layout.addWidget(self.__worker_list)
 
 
-class WorkerModel(QAbstractListModel):
+class WorkerModel(QAbstractTableModel):
     def __init__(self, worker: SchedulerConnectionWorker, parent=None):
         super(WorkerModel, self).__init__(parent)
+        self.__scheduler_worker = worker
         self.__workers: Dict[str, dict] = {}  # address is the key
         self.__order: List[str] = []
         self.__inv_order: Dict[str, int] = {}
+        self.__cols = {'id': 'id', 'last_address': 'address', 'cpu_count': 'cpus', 'mem_size': 'mem', 'gpu_count': 'gpus', 'gmem_size': 'gmem', 'last_seen': 'last seen', 'worker_type': 'type'}
+        self.__cols_order = ('id', 'last_address', 'cpu_count', 'mem_size', 'gpu_count', 'gmem_size', 'last_seen', 'worker_type')
+        assert len(self.__cols) == len(self.__cols_order)
+
+        self.start()
+
+    def headerData(self, section: int, orientation, role: int = Qt.DisplayRole):
+        if orientation == Qt.Vertical:
+            return None
+        if role != Qt.DisplayRole:
+            return None
+        return self.__cols[self.__cols_order[section]]
+
+    def columnCount(self, parent: QModelIndex = None) -> int:
+        return len(self.__cols)
 
     def rowCount(self, parent: QModelIndex = None) -> int:
         return len(self.__workers)
@@ -28,8 +54,13 @@ class WorkerModel(QAbstractListModel):
             return None
         row = index.row()
         col = index.column()
-        # TODO: this is just a placeholder
-        return list(self.__workers[self.__order[row]].items())[col]
+        col_name = self.__cols_order[col]
+        raw_data = self.__workers[self.__order[row]][col_name]
+        if col_name == 'last_seen':
+            return datetime.fromtimestamp(raw_data).strftime('%H:%M:%S %d.%m.%Y')
+        if col_name == 'worker_type':
+            return str(WorkerType(raw_data))
+        return raw_data
 
     @Slot(object)
     def full_update(self, uidata: UiData):
@@ -65,3 +96,9 @@ class WorkerModel(QAbstractListModel):
                 self.__order.remove(key)
                 self.__inv_order = {x: i for i, x in enumerate(self.__order)}
                 self.endRemoveRows()
+
+    def start(self):
+        self.__scheduler_worker.full_update.connect(self.full_update)
+
+    def stop(self):
+        self.__scheduler_worker.disconnect(self)
