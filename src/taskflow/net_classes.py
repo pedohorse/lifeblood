b698405cd2b256bacecce85403d96eb7d5ee7986
pedohorse <xapkohheh@gmail.com>
2021-10-04 23:46:59 +0200

    added WorkerResources holder with what worker can introduce itself to scheduler

--- src/taskflow/net_classes.py
+++ src/taskflow/net_classes.py
@@ -1,3 +1,6 @@
+import uuid
+import psutil
+import pickle
 from typing import TYPE_CHECKING, Type
 if TYPE_CHECKING:
     from basenode import BaseNode
@@ -9,3 +12,19 @@ class NodeTypeMetadata:
         self.label = node_type.label()
         self.tags = set(node_type.tags())
         self.description = node_type.description()
+
+
+class WorkerResources:
+    def __init__(self):
+        self.hwid = uuid.getnode()
+        self.cpu_count = psutil.cpu_count()
+        self.mem_size = psutil.virtual_memory().total
+        self.gpu_count = 0  # TODO: implement this
+        self.gmem_size = 0
+
+    def serialize(self) -> bytes:
+        return pickle.dumps(self)
+
+    @classmethod
+    def deserialize(cls, data) -> "WorkerResources":
+        return pickle.loads(data)
