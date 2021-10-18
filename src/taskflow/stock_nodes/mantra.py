89a13e36e1a9b781c6bd0a39469efccbce9a2035
pedohorse <xapkohheh@gmail.com>
2021-10-19 00:20:16 +0200

    ifd path, image path are now parameters defaulting to file and outimage attribs

--- src/taskflow/stock_nodes/mantra.py
+++ src/taskflow/stock_nodes/mantra.py
@@ -29,6 +29,8 @@ class Mantra(BaseNode):
         ui = self.get_ui()
         with ui.initializing_interface_lock():
             ui.color_scheme().set_main_color(0.5, 0.25, 0.125)
+            ui.add_parameter('ifd path', 'ifd file path', NodeParameterType.STRING, "`task['file']`")
+            ui.add_parameter('image path', 'output image file path', NodeParameterType.STRING, "`task['outimage']`")
 
     def process_task(self, context) -> ProcessingResult:
         args = context.task_attributes()
@@ -37,7 +39,7 @@ class Mantra(BaseNode):
 
         env = InvocationEnvironment()
 
-        invoc = InvocationJob(['mantra', '-V', '2a', args['file'], args['outimage']], env=env)
+        invoc = InvocationJob(['mantra', '-V', '2a', context.param_value('ifd path'), context.param_value('image path')], env=env)
         res = ProcessingResult(invoc)
         return res
 
