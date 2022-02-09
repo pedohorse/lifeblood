945a61b98438e73dc8fcb2ec30102e54c5a0751f
pedohorse <xapkohheh@gmail.com>
2022-02-09 02:41:41 +0100

    started implementing complex plugin structure

--- src/lifeblood/pluginloader.py
+++ src/lifeblood/pluginloader.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 import hashlib
 import importlib.util
 
@@ -14,6 +15,71 @@ __plugin_file_hashes = {}
 logger = logging.get_logger('plugin_loader')
 
 
+def _install_node(filepath, plugin_category):
+    filename = os.path.basename(filepath)
+    filebasename, fileext = os.path.splitext(filename)
+
+    modpath = f'lifeblood.nodeplugins.{plugin_category}.{filebasename}'
+    mod_spec = importlib.util.spec_from_file_location(modpath, filepath)
+    try:
+        mod = importlib.util.module_from_spec(mod_spec)
+        mod_spec.loader.exec_module(mod)
+    except:
+        logger.exception(f'failed to load plugin "{filebasename}". skipping.')
+    for requred_attr in ('node_class',):
+        if not hasattr(mod, requred_attr):
+            logger.error(f'error loading plugin "{filebasename}". '
+                         f'required method {requred_attr} is missing.')
+            continue
+    plugins[mod.node_class().type_name()] = mod
+    hasher = hashlib.md5()
+    with open(filepath, 'rb') as f:
+        hasher.update(f.read())
+    __plugin_file_hashes[mod.node_class().type_name()] = hasher.hexdigest()
+    sys.modules[modpath] = mod
+
+
+def _install_package(package_path, plugin_category):
+    """
+    package structure:
+        [package_name:dir]
+        |_python
+        | |_X           <- these are added to PYTHONPATH based on X.Y
+        | |_X.Y         <-/
+        |_nodes
+        | |_node1.py    <- these are loaded as usual node plugins
+        | |_node2.py    <-/
+        |_whatever_file1.lol
+        |_whatever_dir1
+          |_whatever_file2.lol
+
+    :param package_path:
+    :param plugin_category:
+    :return:
+    """
+    # install extra python modules
+    python_base_path = os.path.join(package_path, 'python')
+    if os.path.exists(python_base_path):
+        sysver = sys.version_info
+        pyvers = [tuple(int(y) for y in x.split('.')) for x in os.listdir(python_base_path) if x.isdigit() or re.match(r'^\d+\.\d+$', x)]
+        pyvers = [x for x in pyvers if x[0] == sysver.major
+                                       and (len(x) < 2 or x[1] == sysver.minor)
+                                       and (len(x) < 3 or x[2] == sysver.micro)]
+        pyvers = sorted(pyvers, key=lambda x: len(x), reverse=True)
+        for pyver in pyvers:
+            sys.path.append(os.path.join(python_base_path, '.'.join(str(x) for x in pyver)))
+
+    # install nodes
+    nodes_path = os.path.join(package_path, 'nodes')
+    if os.path.exists(nodes_path):
+        for filename in os.listdir(nodes_path):
+            filebasename, fileext = os.path.splitext(filename)
+            if fileext != '.py':
+                continue
+            _install_node(os.path.join(nodes_path, filename), plugin_category)
+
+
+
 def init():
     logger.info('loading core plugins')
     global plugins
@@ -25,28 +91,15 @@ def init():
     plugin_paths.append((stock_plugins_path, 'stock'))
     for plugin_path, plugin_category in plugin_paths:
         for filename in os.listdir(plugin_path):
-            filebasename, fileext = os.path.splitext(filename)
-            if fileext != '.py':
-                continue
-            modpath = f'lifeblood.nodeplugins.{plugin_category}.{filebasename}'
-            mod_spec = importlib.util.spec_from_file_location(modpath,
-                                                              os.path.join(plugin_path, filename))
-            try:
-                mod = importlib.util.module_from_spec(mod_spec)
-                mod_spec.loader.exec_module(mod)
-            except:
-                logger.exception(f'failed to load plugin "{filebasename}". skipping.')
-            for requred_attr in ('node_class',):
-                if not hasattr(mod, requred_attr):
-                    logger.error(f'error loading plugin "{filebasename}". '
-                                 f'required method {requred_attr} is missing.')
+            filepath = os.path.join(plugin_path, filename)
+            if os.path.isdir(filepath):
+                _install_package(filepath, plugin_category)
+            else:
+                filebasename, fileext = os.path.splitext(filename)
+                if fileext != '.py':
                     continue
-            plugins[mod.node_class().type_name()] = mod
-            hasher = hashlib.md5()
-            with open(os.path.join(plugin_path, filename), 'rb') as f:
-                hasher.update(f.read())
-            __plugin_file_hashes[mod.node_class().type_name()] = hasher.hexdigest()
-            sys.modules[modpath] = mod
+                _install_node(filepath, plugin_category)
+
     logger.info('loaded plugins:\n' + '\n\t'.join(plugins.keys()))
 
 
