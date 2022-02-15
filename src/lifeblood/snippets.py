498d06a00de059883626d0371ff70bcb3c8c10b2
pedohorse <xapkohheh@gmail.com>
2022-02-15 01:15:03 +0100

    added snippet serialization class

--- src/lifeblood/snippets.py
+++ src/lifeblood/snippets.py
@@ -1,5 +1,6 @@
 import asyncio
-import pickle
+import json
+import dataclasses
 from dataclasses import dataclass
 
 from .enums import NodeParameterType
@@ -11,6 +12,40 @@ class NodeSnippetData:
     """
     class containing enough information to reproduce a certain snippet of nodes, with parameter values and connections ofc
     """
+
+    class Serializer(json.JSONEncoder):
+        def default(self, obj):
+            if dataclasses.is_dataclass(obj):
+                dcs = obj.__dict__  # dataclasses.asdict is recursive, kills inner dataclasses
+                dcs['__dataclass__'] = obj.__class__.__name__
+                return dcs
+            elif isinstance(obj, NodeSnippetData):
+                return {'nodes': obj.nodes_data,
+                        'connections': obj.connections_data,
+                        '__NodeSnippetData__': '==3*E=='
+                        }
+            elif isinstance(obj, NodeParameterType):
+                return {'value': obj.value,
+                        '__NodeParameterType__': '==3*E=='
+                        }
+            return super(NodeSnippetData.Serializer, self).default(obj)
+
+    class Deserializer(json.JSONDecoder):
+        def dedata(self, obj):
+            if '__dataclass__' in obj:
+                data = getattr(NodeSnippetData, obj['__dataclass__'])(**{k: v for k, v in obj.items() if k != '__dataclass__'})
+                if obj['__dataclass__'] == 'NodeData':
+                    data.pos = tuple(data.pos)
+                return data
+            elif obj.get('__NodeSnippetData__', None) == '==3*E==':
+                return NodeSnippetData(obj['nodes'], obj['connections'])
+            elif obj.get('__NodeParameterType__', None) == '==3*E==':
+                return NodeParameterType(obj['value'])
+            return obj
+
+        def __init__(self):
+            super(NodeSnippetData.Deserializer, self).__init__(object_hook=self.dedata)
+
     @dataclass
     class ParamData:
         name: str
@@ -37,19 +72,28 @@ class NodeSnippetData:
         self.nodes_data = list(nodes_data)
         self.connections_data = list(connections_data)
 
-    def serialize(self) -> bytes:
+    def __eq__(self, other: "NodeSnippetData"):
+        return self.nodes_data == other.nodes_data and self.connections_data == other.connections_data
+
+    def __repr__(self):
+        nodesrepr = '\n'.join(repr(x) for x in self.nodes_data)
+        connsrepr = '\n'.join(repr(x) for x in self.connections_data)
+        return f'NodeSnippetData: nodes: {nodesrepr}\n\nconnections: {connsrepr}'
+
+    def serialize(self, ascii=True) -> bytes:
         """
-        serialize into bytes, not ascii-friendly
+        serialize into bytes, ascii-friendly or not
         """
-        return pickle.dumps(self)
+
+        return json.dumps(self, cls=NodeSnippetData.Serializer, indent=4).encode('UTF-8')
 
     async def serialize_async(self) -> bytes:
         return await asyncio.get_event_loop().run_in_executor(None, self.serialize)
 
     @classmethod
     def deserialize(cls, data: bytes) -> "NodeSnippetData":
-        return pickle.loads(data)
+        return json.loads(data.decode('UTF-8'), cls=NodeSnippetData.Deserializer)
 
     @classmethod
-    def deserialize_async(cls, data: bytes) -> "NodeSnippetData":
+    async def deserialize_async(cls, data: bytes) -> "NodeSnippetData":
         return await asyncio.get_event_loop().run_in_executor(None, cls.deserialize, data)
---
+++ tests/test_snippets.py
@@ -0,0 +1,64 @@
+from unittest import TestCase
+from random import Random
+import string
+from lifeblood.snippets import NodeSnippetData
+from lifeblood.enums import NodeParameterType
+
+
+class SerializationTest(TestCase):
+
+    def create_random_data(self, seed) -> NodeSnippetData:
+        rng = Random(seed)
+        nodes = []
+        nodes_count = rng.randint(0, 333)
+        for i in range(nodes_count):
+            params = {}
+            for _ in range(rng.randint(0, 100)):
+                paramname = ''.join(rng.choice(string.ascii_letters) for _ in range(rng.randint(1, 32)))
+                paramtype = rng.choice(list(NodeParameterType))
+                if paramtype == NodeParameterType.STRING:
+                    paramval = ''.join(rng.choice(string.ascii_letters) for _ in range(rng.randint(1, 32)))
+                elif paramtype == NodeParameterType.INT:
+                    paramval = rng.randint(-999999, 999999)
+                elif paramtype == NodeParameterType.BOOL:
+                    paramval = rng.random() > 0.5
+                elif paramtype == NodeParameterType.FLOAT:
+                    paramval = rng.uniform(-999999, 999999)
+                else:
+                    raise NotImplementedError()
+                paramexpr = None
+                if rng.random() > 0.1:
+                    paramexpr = ''.join(rng.choice(string.ascii_letters) for _ in range(rng.randint(1, 256)))
+                params[paramname] = NodeSnippetData.ParamData(paramname, paramtype, paramval, paramexpr)
+            typename = ''.join(rng.choice(string.ascii_letters) for _ in range(rng.randint(1, 32)))
+            name = ''.join(rng.choice(string.ascii_letters) for _ in range(rng.randint(1, 32)))
+            nodes.append(NodeSnippetData.NodeData(i, typename, name, params, (rng.uniform(-100, 100), rng.uniform(-100, 100))))
+        conns = []
+        if nodes_count > 0:
+            for i in range(rng.randint(0, 666)):
+                conns.append(NodeSnippetData.ConnData(rng.randint(0, nodes_count-1),
+                                                      ''.join(rng.choice(string.ascii_letters) for _ in range(rng.randint(1, 32))),
+                                                      rng.randint(0, nodes_count - 1),
+                                                      ''.join(rng.choice(string.ascii_letters) for _ in range(rng.randint(1, 32))),
+                                                      ))
+        return NodeSnippetData(nodes, conns)
+
+    def test_simple(self):
+        x = NodeSnippetData([NodeSnippetData.NodeData(0, 'foo', 'bar',
+                                                      {'qwe': NodeSnippetData.ParamData('asd',
+                                                                                        NodeParameterType.FLOAT,
+                                                                                        0.1,
+                                                                                        None)
+                                                       }, (1.2, 3.4))],
+                            [NodeSnippetData.ConnData(0, 'ass', 0, 'mouth')])
+
+        # print(x.serialize().decode('UTF-8'))
+        self.assertEqual(NodeSnippetData.deserialize(x.serialize()), x)
+
+    def test_random(self):
+        for i in range(16):
+            x = self.create_random_data(i)
+            y = self.create_random_data(i)
+
+            self.assertEqual(x, y)
+            self.assertEqual(NodeSnippetData.deserialize(x.serialize()), x)
