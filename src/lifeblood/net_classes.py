1bdfc96279de4eb08338bf91feed00ff14481809
pedohorse <xapkohheh@gmail.com>
2022-03-28 01:17:08 +0200

    added settings_names as part of metadata

--- src/lifeblood/net_classes.py
+++ src/lifeblood/net_classes.py
@@ -8,10 +8,12 @@ if TYPE_CHECKING:
 
 class NodeTypeMetadata:
     def __init__(self, node_type: Type["BaseNode"]):
+        from . import pluginloader  # here cuz it should only be created from lifeblood, but can be used from viewer too
         self.type_name = node_type.type_name()
         self.label = node_type.label()
         self.tags = set(node_type.tags())
         self.description = node_type.description()
+        self.settings_names = tuple(pluginloader.nodes_settings.get(node_type.type_name(), {}).keys())
 
 
 class WorkerResources:
