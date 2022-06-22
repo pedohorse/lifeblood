b78e3863d64a32317d33ef384b3c8dc1ae76d21d
pedohorse <xapkohheh@gmail.com>
2022-06-22 02:46:10 +0200

    removed res command as workers will not report resources any more, added pulse command

--- src/lifeblood/scheduler_task_protocol.py
+++ src/lifeblood/scheduler_task_protocol.py
@@ -56,6 +56,8 @@ class SchedulerTaskProtocol(asyncio.StreamReaderProtocol):
                     wid = await self.__scheduler.worker_id_from_address(addr)
                     state = await self.__scheduler.get_worker_state(wid)
                     writer.write(struct.pack('>I', state.value))
+                elif command == b'pulse':
+                    writer.write(b'\1')
                 elif command == b'done':
                     tasksize = struct.unpack('>Q', await reader.readexactly(8))[0]
                     task = await reader.readexactly(tasksize)
@@ -71,12 +73,6 @@ class SchedulerTaskProtocol(asyncio.StreamReaderProtocol):
                     stdout = await read_string()
                     stderr = await read_string()
                     await self.__scheduler.task_cancel_reported(task, stdout, stderr)
-                    writer.write(b'\1')
-                elif command == b'res':
-                    addr = await read_string()
-                    reslength = struct.unpack('>Q', await reader.readexactly(8))[0]
-                    worker_resources: WorkerResources = WorkerResources.deserialize(await reader.readexactly(reslength))
-                    await self.__scheduler.update_worker_resources(addr, worker_resources)
                     writer.write(b'\1')
                 elif command == b'hello':
                     # worker reports for duty
@@ -198,6 +194,12 @@ class SchedulerTaskClient:
         assert await self.__reader.readexactly(1) == b'\1'
 
     async def ping(self, my_address: str) -> WorkerState:
+        """
+        remind scheduler about worker's existence and get back what he thinks of us
+
+        :param my_address: address of this worker used to register at scheduler
+        :return: worker state that scheduler thinks a worker with given address has
+        """
         await self._ensure_conn_open()
         self.__writer.write(b'ping\n')
         try:
@@ -208,15 +210,21 @@ class SchedulerTaskClient:
             self.__logger.error('ping failed. %s', e)
             raise
 
-    async def update_resources(self, address_of_worker: str, resources: WorkerResources):
+    async def pulse(self) -> None:
+        """
+        just ping the scheduler and get back a response, check if it's alive
+        check pulse sorta
+
+        :return:
+        """
         await self._ensure_conn_open()
-        self.__writer.write(b'res\n')
-        await self.write_string(address_of_worker)
-        resdata = resources.serialize()
-        self.__writer.write(struct.pack('>Q', len(resdata)))
-        self.__writer.write(resdata)
-        await self.__writer.drain()
-        assert await self.__reader.readexactly(1) == b'\1'
+        self.__writer.write(b'pulse\n')
+        try:
+            await self.__writer.drain()
+            await self.__reader.readexactly(1)
+        except ConnectionResetError as e:
+            self.__logger.error('pulse check failed. %s', e)
+            raise
 
     async def say_hello(self, address_to_advertise: str, worker_type: WorkerType, worker_resources: WorkerResources):
         await self._ensure_conn_open()
