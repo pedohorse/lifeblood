ca6054b8e17a477bbfb287ea57c15095fa5f9fa4
pedohorse <xapkohheh@gmail.com>
2022-07-14 23:20:52 +0200

    added more file operations: cp, mv

--- src/lifeblood/stock_nodes/fileop.py
+++ src/lifeblood/stock_nodes/fileop.py
@@ -23,12 +23,16 @@ class FileOperations(BaseNode):
             with ui.multigroup_parameter_block('item count'):
                 with ui.parameters_on_same_line_block():
                     ui.add_parameter('path', 'do', NodeParameterType.STRING, '')
-                    ui.add_parameter('op', 'operation', NodeParameterType.STRING, 'nop') \
+                    opparam = ui.add_parameter('op', 'operation', NodeParameterType.STRING, 'nop') \
                         .add_menu((('nothing', 'nop'),
                                    ('create', 'touch'),
                                    ('create dir', 'mkdir'),
                                    ('create base dir', 'mkdirbase'),
-                                   ('delete', 'rm')))
+                                   ('delete', 'rm'),
+                                   ('copy', 'cp'),
+                                   ('move', 'mv')))
+                    ui.add_parameter('other path', 'to here', NodeParameterType.STRING, '') \
+                        .append_visibility_condition(opparam, 'in', ('cp', 'mv'))
 
     @classmethod
     def label(cls) -> str:
@@ -61,15 +65,26 @@ class FileOperations(BaseNode):
                     scriptlines.append(f'os.makedirs({repr(path)}, exist_ok=True)')
                 elif op == 'mkdirbase':
                     scriptlines.append(f'os.makedirs(os.path.dirname({repr(path)}, exist_ok=True)')
-                elif op == 'rm':
-                    scriptlines.extend(
-                        [f'if os.path.exists({repr(path)}):',
-                         f'    if os.path.isfile({repr(path)}):',
-                         f'        os.remove({repr(path)})',
-                         f'    elif os.path.isdir({repr(path)}):',
-                         f'        shutil.rmtree({repr(path)})',
-                         f'    else:',
-                         f'        raise RuntimeError(f\'cannot determine is "{path}" a file or a dir\')'])
+                elif op in ('cp', 'mv', 'rm'):
+                    if op in ('cp', 'mv'):
+                        other_path = os.path.realpath(context.param_value(f'other path_{i}'))
+                        scriptlines.extend(
+                            [f'if os.path.isfile({repr(path)}):',
+                             f'    shutil.copy2({repr(path)}, {repr(other_path)})',
+                             f'elif os.path.isdir(path):',
+                             f'    shutil.copytree(path, other_path, dirs_exist_ok=True)',
+                             f'else:',
+                             f'    raise RuntimeError(\'cannot determine is "{path}" a file or a dir\')']
+                        )
+                    if op in ('mv', 'rm'):
+                        scriptlines.extend(
+                            [f'if os.path.exists({repr(path)}):',
+                             f'    if os.path.isfile({repr(path)}):',
+                             f'        os.remove({repr(path)})',
+                             f'    elif os.path.isdir({repr(path)}):',
+                             f'        shutil.rmtree({repr(path)})',
+                             f'    else:',
+                             f'        raise RuntimeError(\'cannot determine is "{path}" a file or a dir\')'])
                 else:
                     raise ProcessingError(f'unknown operation: "{op}"')
             if len(scriptlines) == 0:
@@ -95,16 +110,32 @@ class FileOperations(BaseNode):
                 elif op == 'mkdirbase':
                     os.makedirs(os.path.dirname(path), exist_ok=True)
                 elif op == 'rm':
-                    if os.path.exists(path):
-                        if os.path.isfile(path):
-                            os.remove(path)
-                        elif os.path.isdir(path):
-                            shutil.rmtree(path)
-                        else:
-                            raise ProcessingError(f'cannot determine is "{path}" a file or a dir')
+                    delete_path(path)
+                elif op in ('cp', 'mv'):
+                    other_path = os.path.realpath(context.param_value(f'other path_{i}'))
+                    os.makedirs(os.path.dirname(other_path), exist_ok=True)
+                    if os.path.isfile(path):
+                        shutil.copy2(path, other_path)
+                    elif os.path.isdir(path):
+                        shutil.copytree(path, other_path, dirs_exist_ok=True)
+                    else:
+                        raise ProcessingError(f'cannot determine is "{path}" a file or a dir')
+                    if op == 'mv':
+                        delete_path(path)
                 else:
                     raise ProcessingError(f'unknown operation: "{op}"')
         return ProcessingResult()
 
     def postprocess_task(self, context: ProcessingContext) -> ProcessingResult:
         return ProcessingResult()
+
+
+def delete_path(path):
+    if not os.path.exists(path):
+        return
+    if os.path.isfile(path):
+        os.remove(path)
+    elif os.path.isdir(path):
+        shutil.rmtree(path)
+    else:
+        raise ProcessingError(f'cannot determine is "{path}" a file or a dir')
