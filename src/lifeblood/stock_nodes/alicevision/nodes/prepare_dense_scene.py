685731881f9cd2d4fa40ab3901975eb94c777eb5
pedohorse <xapkohheh@gmail.com>
2022-08-20 11:44:52 +0200

    wip

--- src/lifeblood/stock_nodes/alicevision/nodes/prepare_dense_scene.py
+++ src/lifeblood/stock_nodes/alicevision/nodes/prepare_dense_scene.py
@@ -16,6 +16,36 @@ class AlicevisionPrepareDenseScene(AlicevisionBaseNode):
         ui = self.get_ui()
         with ui.initializing_interface_lock():
             ui.add_parameter('input', 'Input Images Directory', NodeParameterType.STRING, "`task['av_structure_from_motion']`")
-            ui.add_parameter('', 'Images Folders', NodeParameterType.STRING, "")  # TODO: make a set of checkboxes
             ui.add_parameter('output', 'Output Image Folder', NodeParameterType.STRING, "`config['global_scratch_location']`/alicevision/`task['uuid']`/PrepareDenseScene")
-            ui.add_parameter('output', 'Output Undistorted Images', NodeParameterType.STRING, "`config['global_scratch_location']`/alicevision/`task['uuid']`/StructureFromMotion/sfm.abc")
+            # ui.add_parameter('', 'Output Undistorted Images', NodeParameterType.STRING, "`config['global_scratch_location']`/alicevision/`task['uuid']`/StructureFromMotion/sfm.abc")
+
+            ui.add_separator()
+            with ui.parameters_on_same_line_block():
+                ui.add_parameter('rangeStart', 'Image Range Start', NodeParameterType.INT, 0)
+                ui.add_parameter('rangeSize', 'Image Chunk Size', NodeParameterType.INT, 999999)
+
+    @classmethod
+    def label(cls) -> str:
+        return 'AV Prepare Dense Scene'
+
+    @classmethod
+    def tags(cls) -> Iterable[str]:
+        return 'alicevision', 'dense', 'scene', 'prepare'
+
+    @classmethod
+    def type_name(cls) -> str:
+        return 'stock_alicevision_prepare_dense_scene'
+
+    @classmethod
+    def description(cls) -> str:
+        return 'this node type does not have a description'
+
+    def process_task(self, context: ProcessingContext) -> ProcessingResult:
+        output = Path(context.param_value('output').strip())
+        if not output.is_absolute():
+            raise ProcessingError('output path must be absolute and not empty')
+
+        output.mkdir(parents=True, exist_ok=True)
+
+        args = ['aliceVision_prepareDenseScene', '--verboseLevel', 'info']
+        args += ['--input']
