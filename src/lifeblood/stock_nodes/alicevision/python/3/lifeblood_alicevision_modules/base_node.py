349b2eaf33869ccce8e14e9eec06ca0f2e94c08b
pedohorse <xapkohheh@gmail.com>
2022-08-21 14:50:50 +0200

    removed alicevision_root as it doesnt make sense on scheduler side

--- src/lifeblood/stock_nodes/alicevision/python/3/lifeblood_alicevision_modules/base_node.py
+++ src/lifeblood/stock_nodes/alicevision/python/3/lifeblood_alicevision_modules/base_node.py
@@ -9,8 +9,3 @@ class AlicevisionBaseNode(BaseNodeWithTaskRequirements):
         ui = self.get_ui()
         with ui.initializing_interface_lock():
             ui.color_scheme().set_main_color(0.15, 0.15, 0.6)
-
-    @staticmethod
-    def alicevision_root() -> Path:
-        # TODO: this doesn't work stupid! not from scheduler anyway
-        return Path(shutil.which('aliceVision_cameraInit')).parent.parent  # executable is in [root]/bin/smth
