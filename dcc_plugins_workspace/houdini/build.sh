29f68f9bbb01b0b7619ed39312fafc2777db954c
pedohorse <xapkohheh@gmail.com>
2022-11-20 00:46:47 +0100

    modified to take lifeblood_client from src

--- dcc_plugins_workspace/houdini/build.sh
+++ dcc_plugins_workspace/houdini/build.sh
@@ -2,3 +2,8 @@
 
 # assume houdini environment is initialized
 hotl -l otls/Driver-lifeblood_submitter-1.0.0.hda ../../dcc_plugins/houdini/otls/Driver-lifeblood_submitter-1.0.0.hda
+
+# copy client module
+rsync -arhv ../../src/lifeblood_client ../../dcc_plugins/houdini/python2.7libs
+rsync -arhv ../../src/lifeblood_client ../../dcc_plugins/houdini/python3.7libs
+rsync -arhv ../../src/lifeblood_client ../../dcc_plugins/houdini/python3.9libs
