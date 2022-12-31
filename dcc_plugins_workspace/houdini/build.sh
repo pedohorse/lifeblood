f7dd875d3e9149cd8f5bd279f9a367d6d42b06f6
pedohorse <xapkohheh@gmail.com>
2022-12-31 01:56:38 +0100

    added blender python module lifeblood_client updated build script

--- dcc_plugins_workspace/houdini/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# assume houdini environment is initialized
-hotl -l otls/Driver-lifeblood_submitter-1.0.0.hda ../../dcc_plugins/houdini/otls/Driver-lifeblood_submitter-1.0.0.hda
-
-# copy client module
-rsync -arhv --exclude=__pycache__ --exclude="*.pyc" ../../src/lifeblood_client ../../dcc_plugins/houdini/python2.7libs
-rsync -arhv --exclude=__pycache__ --exclude="*.pyc" ../../src/lifeblood_client ../../dcc_plugins/houdini/python3.7libs
-rsync -arhv --exclude=__pycache__ --exclude="*.pyc" ../../src/lifeblood_client ../../dcc_plugins/houdini/python3.9libs
