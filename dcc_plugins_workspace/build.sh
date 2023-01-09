cd9dc935928c446b5293c0e9a31e19d154ae64f9
pedohorse <xapkohheh@gmail.com>
2023-01-09 13:16:11 +0100

    updated build script to build blender addon

--- dcc_plugins_workspace/build.sh
+++ dcc_plugins_workspace/build.sh
@@ -7,4 +7,11 @@ hotl -l houdini/otls/Driver-lifeblood_submitter-1.0.0.hda ../dcc_plugins/houdini
 rsync -arhv --exclude=__pycache__ --exclude="*.pyc" ../src/lifeblood_client ../dcc_plugins/houdini/python2.7libs
 rsync -arhv --exclude=__pycache__ --exclude="*.pyc" ../src/lifeblood_client ../dcc_plugins/houdini/python3.7libs
 rsync -arhv --exclude=__pycache__ --exclude="*.pyc" ../src/lifeblood_client ../dcc_plugins/houdini/python3.9libs
-rsync -arhv --exclude=__pycache__ --exclude="*.pyc" ../src/lifeblood_client ../dcc_plugins/blender/python
+rsync -arhv --exclude=__pycache__ --exclude="*.pyc" ../src/lifeblood_client ../dcc_plugins/blender/blender_path/addons/scripts
+
+# post process
+
+# zip blender addon
+pushd ../dcc_plugins/blender/blender_path/addons
+zip -r ../../lifeblood_addon.zip lifeblood_plugin --exclude "*__pycache__/" --exclude "*.pyc"
+popd
