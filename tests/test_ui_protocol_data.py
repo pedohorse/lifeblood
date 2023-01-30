7060e37d903498ecc7ee10f74c1eb0abad5231c4
pedohorse <xapkohheh@gmail.com>
2023-01-30 17:30:13 +0100

    trivial test data update

--- tests/test_ui_protocol_data.py
+++ tests/test_ui_protocol_data.py
@@ -1,15 +1,33 @@
 from unittest import TestCase
 from io import BytesIO
+from lifeblood.enums import TaskState, WorkerType, WorkerState, TaskGroupArchivedState
 from lifeblood.ui_protocol_data import UiData, UIDataType, NodeGraphStructureData, \
-    NodeData, NodeConnectionData, TaskBatchData
-
+    NodeData, NodeConnectionData, TaskBatchData, TaskData, WorkerData, WorkerResources, WorkerBatchData, \
+    TaskGroupData, TaskGroupStatisticsData
+import pickle
 
 class Tests(TestCase):
     def test_trivial1(self):
         exp_uid = UiData(UIDataType.FULL,
-                         123456,
-                         NodeGraphStructureData({}, {}),
-                         TaskBatchData({}), None, {})
+            123456,
+            NodeGraphStructureData({
+                1: NodeData(1, "name1", "supertype"),
+                22: NodeData(22, "foo", "megatype")
+            }, {
+                32: NodeConnectionData(32, 321, "mama", 234, "papa"),
+                41: NodeConnectionData(41, 123, "quaq", 919, "guac")
+            }),
+            TaskBatchData({
+                11: TaskData(11, None, 10, 5, TaskState.ERROR, "oh noooo", True, 1531, "main", "out",
+                             'a task', 2, 11, 52.1, 8484, 158, 816, {'groo', 'froo'})
+            }), WorkerBatchData({
+                121: WorkerData(121, WorkerResources(12.3, 23.4, 929283838, 939384848, 56.7, 67.8, 84847575, 85857676),
+                                '1928374', '127.1.2.333:blo', 1234567, WorkerState.BUSY, WorkerType.STANDARD,
+                                1534, 8273, 7573, 55.5, {"borker", "gorker"})
+            }), {
+                'grooup foo': TaskGroupData('grooup foo', 2345678, TaskGroupArchivedState.ARCHIVED, 22.3,
+                                            TaskGroupStatisticsData(45, 56, 67, 78))
+            })
 
         buffer = BytesIO()
         buffer.write(b'cat')
