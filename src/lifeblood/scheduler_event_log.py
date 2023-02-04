46f32460d7c2cc1795e72f4de91433aa16f4c4a2
pedohorse <xapkohheh@gmail.com>
2023-02-04 01:31:26 +0100

    better

--- src/lifeblood/scheduler_event_log.py
+++ src/lifeblood/scheduler_event_log.py
@@ -1,24 +1,10 @@
-from dataclasses import dataclass, field
 import logging
-import time
-from .ui_protocol_data import TaskData
-from .enums import UIEventType
+from .ui_events import SchedulerEvent
 
-from typing import Dict, List
+from typing import Dict, List, Union
 
 
 logger = logging.getLogger(__name__)
-
-@dataclass
-class SchedulerEvent:
-    timestamp: float = field(default_factory=lambda: time.time(), init=False)
-    event_id: int
-    event_type: UIEventType
-
-
-@dataclass
-class TaskEvent(SchedulerEvent):
-    task_data: TaskData
 
 
 class SchedulerEventLog:
@@ -28,6 +14,8 @@ class SchedulerEventLog:
 
         self.__events: List[SchedulerEvent] = []  # will be kept sorted
         self.__event_id_to_event: Dict[int, SchedulerEvent] = {}
+
+        self.__internal_event_counter_next = 0
 
     def __find_index_below_timestamp(self, timestamp):
         if len(self.__events) < 8:
@@ -39,6 +27,10 @@ class SchedulerEventLog:
         def _find_bin(l: List[SchedulerEvent], timestamp):
             if len(l) == 1:
                 return -1 if timestamp < l[0].timestamp else 0
+            if timestamp < l[0].timestamp:
+                return -1
+            elif timestamp > l[-1].timestamp:
+                return len(l)-1
             si = len(l) // 2
             if (i := _find_bin(l[si:], timestamp)) > -1:
                 return i + si
@@ -46,18 +38,25 @@ class SchedulerEventLog:
 
         return _find_bin(self.__events, timestamp)
 
-    def append(self, event: SchedulerEvent, do_trim=True):
-        if event.event_id in self.__event_id_to_event:
-            if event != self.__event_id_to_event[event.event_id]:
-                raise RuntimeError('attempting to add event with the same id, but different data')
-            logger.warning(f'ignoring event as it is already in the log: {event}')
-            return
-        i = self.__find_index_below_timestamp(event.timestamp) + 1
-        if i > 0 and self.__events[i-1].event_id >= event.event_id \
-                or i < len(self.__events) - 1 and self.__events[i+1].event_id <= event.event_id:  # sanity checks
-            raise RuntimeError('event IDs are expected to be sorted the same way as their timestamps')
+    def add_event(self, event: SchedulerEvent, do_trim=True):
+        # first check the trivial case
+        if len(self.__events) == 0 or event.event_id > self.__events[-1].event_id and event.timestamp > self.__events[-1].timestamp:
+            i = len(self.__events)
+        else:
+            if event.event_id in self.__event_id_to_event:
+                if event != self.__event_id_to_event[event.event_id]:
+                    raise RuntimeError('attempting to add event with the same id, but different data')
+                logger.warning(f'ignoring event as it is already in the log: {event}')
+                return
+            i = self.__find_index_below_timestamp(event.timestamp) + 1
+            if i > 0 and self.__events[i-1].event_id >= event.event_id \
+                    or i < len(self.__events) - 1 and self.__events[i+1].event_id <= event.event_id:  # sanity checks
+                raise RuntimeError('event IDs are expected to be sorted the same way as their timestamps')
+        if event.event_id < 0:
+            event.event_id = self.__internal_event_counter_next
         self.__events.insert(i, event)
         self.__event_id_to_event[event.event_id] = event
+        self.__internal_event_counter_next = max(self.__internal_event_counter_next, event.event_id) + 1
         if do_trim:
             self.trim()
 
@@ -65,6 +64,8 @@ class SchedulerEventLog:
         if len(self.__events) == 0:
             return
         threshold = self.__events[-1].timestamp - self.__max_time
+        if len(self.__events) < self.__max_count and self.__events[0].timestamp > threshold:
+            return
         trimpos = self.__find_index_below_timestamp(threshold)+1
         for event in self.__events[:trimpos]:
             self.__event_id_to_event.pop(event.event_id)
