b3e1f23bb737ac48013d73d4ef4c09469f80cf82
pedohorse <xapkohheh@gmail.com>
2023-02-19 22:26:51 +0100

    added tiny sleeps for more coarse test timers, speed benchmarks reworked

--- tests/test_scheduler_event_log.py
+++ tests/test_scheduler_event_log.py
@@ -14,34 +14,45 @@ class Tests(TestCase):
         startstamp = time.time_ns()
         log.add_event(SchedulerEvent(2, UIEventType.UPDATE))
         self.assertEqual(1, len(log))
+        time.sleep(0.000001)  # cicd test machines seem to may provide a very coarse time resolution, so we ensure
         log.add_event(SchedulerEvent(3, UIEventType.UPDATE))
         self.assertEqual(2, len(log))
+        time.sleep(0.000001)
         log.add_event(SchedulerEvent(5, UIEventType.UPDATE))
         self.assertEqual(3, len(log))
+        time.sleep(0.000001)
         test_timestamp0 = time.time_ns()       # test timestamp 0
         log.add_event(SchedulerEvent(6, UIEventType.UPDATE))
         self.assertEqual(4, len(log))
+        time.sleep(0.000001)
         missed_event0 = SchedulerEvent(10, UIEventType.UPDATE)
         log.add_event(SchedulerEvent(16, UIEventType.UPDATE))
         self.assertEqual(5, len(log))
+        time.sleep(0.000001)
 
         evs = log.get_since_event(1)  # non existing events should work properly
         print(f'start:{startstamp} ev0:{evs[0].timestamp} ev-1:{evs[-1].timestamp} end:{time.time_ns()}')
         self.assertEqual(5, len(evs))
+        time.sleep(0.000001)
         evs = log.get_since_event(2)
         self.assertEqual(4, len(evs))
+        time.sleep(0.000001)
         evs = log.get_since_event(4)  # non existing events should work properly
         self.assertEqual(3, len(evs))
+        time.sleep(0.000001)
         evs = log.get_since_event(9)  # non existing events should work properly
         self.assertEqual(1, len(evs))
+        time.sleep(0.000001)
 
         self.assertEqual((2, 3, 5, 6, 16), tuple(x.event_id for x in log.get_since_timestamp_ns(startstamp)))
         self.assertEqual((6, 16), tuple(x.event_id for x in log.get_since_timestamp_ns(test_timestamp0)))
         log.add_event(SchedulerEvent(22, UIEventType.UPDATE))
         self.assertEqual(6, len(log))
+        time.sleep(0.000001)
         test_timestamp1 = time.time_ns()       # test timestamp 1
         log.add_event(SchedulerEvent(23, UIEventType.UPDATE))
         self.assertEqual(6, len(log))
+        time.sleep(0.000001)
         missed_event1 = SchedulerEvent(24, UIEventType.UPDATE)
         event = SchedulerEvent(25, UIEventType.UPDATE)
         log.add_event(event)
@@ -52,12 +63,14 @@ class Tests(TestCase):
 
         log.add_event(missed_event0)
         self.assertEqual(6, len(log))
+        time.sleep(0.000001)
         self.assertEqual((6, 10, 16, 22, 23, 25), tuple(x.event_id for x in log.get_since_timestamp_ns(startstamp)))
         self.assertEqual((6, 10, 16, 22, 23, 25), tuple(x.event_id for x in log.get_since_timestamp_ns(test_timestamp0)))
         self.assertEqual((23, 25), tuple(x.event_id for x in log.get_since_timestamp_ns(test_timestamp1)))
 
         log.add_event(missed_event1)
         self.assertEqual(6, len(log))
+        time.sleep(0.000001)
         self.assertEqual((10, 16, 22, 23, 24, 25), tuple(x.event_id for x in log.get_since_timestamp_ns(startstamp)))
         self.assertEqual((10, 16, 22, 23, 24, 25), tuple(x.event_id for x in log.get_since_timestamp_ns(test_timestamp0)))
         self.assertEqual((23, 24, 25), tuple(x.event_id for x in log.get_since_timestamp_ns(test_timestamp1)))
@@ -105,28 +118,40 @@ class Tests(TestCase):
         log.add_event(SchedulerEvent(10, UIEventType.UPDATE))
         self.assertEqual(1, len(log))
 
-    def test_benchmark1(self):
-        # a sanity check more like
+    def _run_iters(self, count, log_time_length_max, log_event_count_max):
         log = SchedulerEventLog(log_time_length_max=10, log_event_count_max=9999999)
+        # pre-create shit for higher precision of actual add_event
+        events = []
+        for i in range(count):
+            events.append(SchedulerEvent(i, UIEventType.UPDATE))
 
+        # now measure
         perf0 = time.perf_counter()
-        for i in range(1000):
-            log.add_event(SchedulerEvent(i, UIEventType.UPDATE), do_trim=True)
+        for ev in events:
+            log.add_event(ev, do_trim=True)
         perf1 = time.perf_counter()
-        delta = perf1 - perf0
-        print(delta)
+        return perf1 - perf0
 
-        self.assertLess(delta, 0.01)
+    def _test_benchmark(self, log_time_length_max, log_event_count_max):
+        # the point of this test is to ensure asymptotic complexity is no worse than linear for the common case
+
+        delta1 = 0
+        for _ in range(10):
+            delta1 += self._run_iters(1000, 10, 9999999)
+        delta1 /= 10
+        delta2 = self._run_iters(10000, 10, 9999999)
+        delta3 = self._run_iters(100000, 10, 9999999)
+        delta4 = self._run_iters(1000000, 10, 9999999)
+        print(f'{delta1} : {delta2} : {delta3} : {delta4}')
+        self.assertGreater(delta1*(10+1), delta2, )
+        self.assertGreater(delta1*(100+10), delta3)
+        self.assertGreater(delta1*(1000+100), delta4)
+
+    def test_benchmark1(self):
+        self._test_benchmark(10, 9999999)
 
     def test_benchmark2(self):
-        # a sanity check more like
-        log = SchedulerEventLog(log_time_length_max=10, log_event_count_max=100)
+        self._test_benchmark(10, 100)
 
-        perf0 = time.perf_counter()
-        for i in range(1000):  # truncations will start after first 100 events
-            log.add_event(SchedulerEvent(i, UIEventType.UPDATE), do_trim=True)
-        perf1 = time.perf_counter()
-        delta = perf1 - perf0
-        print(delta)
-
-        self.assertLess(delta, 0.01)
+    def test_benchmark3(self):
+        self._test_benchmark(None, None)
