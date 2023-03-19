685b62fad39e2b8750221f589f25691c01077492
pedohorse <xapkohheh@gmail.com>
2023-03-19 02:11:49 +0100

    copying matrix-commander install script

--- matrixclient/compile.sh
+++ matrixclient/compile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-zip -r matrixclient.pyz __main__.py matrix_client/ requests/ urllib3/
+zip -r matrixclient.pyz __main__.py matrix_client/ requests/ urllib3/ canonicaljson/
 mv matrixclient.pyz ../src/lifeblood/stock_nodes/matrixclient/data/.
+cp install_matrix_commander.py ../src/lifeblood/stock_nodes/matrixclient/data/.
 printf "all done\n"
