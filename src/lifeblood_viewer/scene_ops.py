92a19f993d3b533e67a2cccf9a1adef5ecb13244
pedohorse <xapkohheh@gmail.com>
2023-04-08 22:59:06 +0200

    adding do methods, fixed parameter change to use sid instead of id

--- src/lifeblood_viewer/scene_ops.py
+++ src/lifeblood_viewer/scene_ops.py
@@ -45,7 +45,7 @@ class CreateNodesOp(UndoableOperation):
             node_ids = [x for x in (self.__scene._session_node_id_to_id(sid) for sid in self.__node_sids) if x is not None]
             if not node_ids:
                 return
-            self.__scene.request_remove_nodes(node_ids, LongOperationData(longop))
+            self.__scene._request_remove_nodes(node_ids, LongOperationData(longop))
             yield
             if callback:
                 callback(self)
@@ -83,6 +83,15 @@ class RemoveNodesOp(UndoableOperation):
         self.__node_sids = node_sids
         self.__restoration_snippet = restoration_snippet
 
+    def do(self, callback: Optional[Callable[["UndoableOperation"], None]] = None):
+        def doop(longop):
+            node_ids = [self.__scene._session_node_id_to_id(sid) for sid in self.__node_sids]
+            self.__scene._request_remove_nodes(node_ids, LongOperationData(longop))
+            yield
+            if callback:
+                callback(self)
+        self.__scene.add_long_operation(doop)
+
     def undo(self, callback: Optional[Callable[["UndoableOperation"], None]] = None):
         def undoop(longop):
             self.__scene.nodes_from_snippet(self.__restoration_snippet, QPointF(*self.__restoration_snippet.pos), longop)
@@ -103,10 +112,20 @@ class RenameNodeOp(UndoableOperation):
         self.__old_name = old_name
         self.__new_name = new_name
 
+    def do(self, callback: Optional[Callable[["UndoableOperation"], None]] = None):
+        def doop(longop):
+            node_id = self.__scene._session_node_id_to_id(self.__node_sid)
+            self.__scene._request_set_node_name(node_id, self.__new_name, LongOperationData(longop))
+            yield
+            if callback:
+                callback(self)
+
+        self.__scene.add_long_operation(doop)
+
     def undo(self, callback: Optional[Callable[["UndoableOperation"], None]] = None):
         def undoop(longop):
             node_id = self.__scene._session_node_id_to_id(self.__node_sid)
-            self.__scene.request_set_node_name(node_id, self.__old_name, LongOperationData(longop))
+            self.__scene._request_set_node_name(node_id, self.__old_name, LongOperationData(longop))
             yield
             if callback:
                 callback(self)
@@ -120,6 +139,16 @@ class MoveNodesOp(UndoableOperation):
     def __init__(self, scene: "QGraphicsImguiScene", info: Iterable[Tuple[int, QPointF, QPointF]]):
         self.__scene = scene
         self.__node_info = tuple(info)
+
+    def do(self, callback: Optional[Callable[["UndoableOperation"], None]] = None):
+        for node_sid, old_pos, new_pos in self.__node_info:
+            node_id = self.__scene._session_node_id_to_id(node_sid)
+            node = self.__scene.get_node(node_id)
+            if node is None:
+                continue
+            node.setPos(new_pos)
+        if callback:
+            callback(self)
 
     def undo(self, callback: Optional[Callable[["UndoableOperation"], None]] = None):
         for node_sid, old_pos, new_pos in self.__node_info:
@@ -143,6 +172,21 @@ class AddConnectionOp(UndoableOperation):
         self.__in_sid = in_node_sid
         self.__in_name = in_name
 
+    def do(self, callback: Optional[Callable[["UndoableOperation"], None]] = None):
+        def doop(longop):
+            out_id = self.__scene._session_node_id_to_id(self.__out_sid)
+            in_id = self.__scene._session_node_id_to_id(self.__in_sid)
+            if out_id is None or in_id is None \
+              or self.__scene.get_node(out_id) is None \
+              or self.__scene.get_node(in_id) is None:
+                logger.warning(f'could not perform op: nodes not found {out_id}, {in_id}')
+                return
+            self.__scene._request_node_connection_add(out_id, self.__out_name, in_id, self.__in_name, LongOperationData(longop))
+            yield
+            if callback:
+                callback(self)
+        self.__scene.add_long_operation(doop)
+
     def undo(self, callback: Optional[Callable[["UndoableOperation"], None]] = None):
         def undoop(longop):
             out_id = self.__scene._session_node_id_to_id(self.__out_sid)
@@ -156,7 +200,7 @@ class AddConnectionOp(UndoableOperation):
             if con is None:
                 logger.warning('could not perform undo: added connection not found')
                 return
-            self.__scene.request_node_connection_remove(con.get_id(), LongOperationData(longop))
+            self.__scene._request_node_connection_remove(con.get_id(), LongOperationData(longop))
             yield
             if callback:
                 callback(self)
@@ -174,6 +218,25 @@ class RemoveConnectionOp(UndoableOperation):
         self.__in_sid = in_node_sid
         self.__in_name = in_name
 
+    def do(self, callback: Optional[Callable[["UndoableOperation"], None]] = None):
+        def doop(longop):
+            out_id = self.__scene._session_node_id_to_id(self.__out_sid)
+            in_id = self.__scene._session_node_id_to_id(self.__in_sid)
+            if out_id is None or in_id is None \
+                    or self.__scene.get_node(out_id) is None \
+                    or self.__scene.get_node(in_id) is None:
+                logger.warning(f'could not perform op: added connection not found: {out_id} {in_id}')
+                return
+            con = self.__scene.get_node_connection_from_ends(out_id, self.__out_name, in_id, self.__in_name)
+            if con is None:
+                logger.warning(f'could not perform op: added connection not found for {out_id}, {self.__out_name}, {in_id}, {self.__in_name}')
+                return
+            self.__scene._request_node_connection_remove(con.get_id(), LongOperationData(longop))
+            yield
+            if callback:
+                callback(self)
+        self.__scene.add_long_operation(doop)
+
     def undo(self, callback: Optional[Callable[["UndoableOperation"], None]] = None):
         def undoop(longop):
             out_id = self.__scene._session_node_id_to_id(self.__out_sid)
@@ -183,22 +246,22 @@ class RemoveConnectionOp(UndoableOperation):
               or self.__scene.get_node(in_id) is None:
                 logger.warning('could not perform undo: added connection not found')
                 return
-            self.__scene.request_node_connection_add(out_id, self.__out_name, in_id, self.__in_name, LongOperationData(longop))
+            self.__scene._request_node_connection_add(out_id, self.__out_name, in_id, self.__in_name, LongOperationData(longop))
             yield
             if callback:
                 callback(self)
         self.__scene.add_long_operation(undoop)
 
     def __str__(self):
-        return f'Wire Remove {self.__out_sid}:{self.__out_name}->{self.__in_sid}:{self.__in_sid}'
+        return f'Wire Remove {self.__out_sid}:{self.__out_name}->{self.__in_sid}:{self.__in_name}'
 
 
 class ParameterChangeOp(UndoableOperation):
-    def __init__(self, scene: "QGraphicsImguiScene", node_id: int, parameter_name: str, new_value=..., new_expression=...):
+    def __init__(self, scene: "QGraphicsImguiScene", node_sid: int, parameter_name: str, new_value=..., new_expression=...):
         """
 
         :param scene:
-        :param node_id:
+        :param node_sid:
         :param parameter:
         :param new_value: ...(Ellipsis) means no change
         :param new_expression: ...(Ellipsis) means no change
@@ -206,22 +269,23 @@ class ParameterChangeOp(UndoableOperation):
         super().__init__()
         self.__scene = scene
         self.__param_name = parameter_name
-        param = scene.get_node(node_id).get_nodeui().parameter(parameter_name)
+        param = scene.get_node(scene._session_node_id_to_id(node_sid)).get_nodeui().parameter(parameter_name)
         self.__old_value = param.unexpanded_value() if new_value is not ... else ...
         self.__old_expression = param.expression() if new_expression is not ... else ...
         self.__new_value = new_value
         self.__new_expression = new_expression
-        self.__node_id = node_id
+        self.__node_sid = node_sid
 
     def do(self, callback: Optional[Callable[["UndoableOperation"], None]] = None):
         def doop(longop):
-            param = self.__scene.get_node(self.__node_id).get_nodeui().parameter(self.__param_name)
+            node_id = self.__scene._session_node_id_to_id(self.__node_sid)
+            param = self.__scene.get_node(node_id).get_nodeui().parameter(self.__param_name)
             if self.__new_value is not ...:
                 param.set_value(self.__new_value)
             if self.__new_expression is not ...:
                 param.set_expression(self.__new_expression)
-            self.__scene._send_node_parameters_change(self.__node_id, [param], LongOperationData(longop))
-            node = self.__scene.get_node(self.__node_id)
+            self.__scene._send_node_parameters_change(node_id, [param], LongOperationData(longop))
+            node = self.__scene.get_node(node_id)
             if node:
                 node.update_ui()
             yield
@@ -231,15 +295,16 @@ class ParameterChangeOp(UndoableOperation):
 
     def undo(self, callback: Optional[Callable[["UndoableOperation"], None]] = None):
         def undoop(longop):
-            param = self.__scene.get_node(self.__node_id).get_nodeui().parameter(self.__param_name)
+            node_id = self.__scene._session_node_id_to_id(self.__node_sid)
+            param = self.__scene.get_node(node_id).get_nodeui().parameter(self.__param_name)
             if self.__old_value is not ...:
                 param.set_value(self.__old_value)
             if self.__old_expression is not ...:
                 param.set_expression(self.__old_expression)
-            self.__scene._send_node_parameters_change(self.__node_id, [param], LongOperationData(longop))
+            self.__scene._send_node_parameters_change(node_id, [param], LongOperationData(longop))
             yield
             # update node ui, just in case
-            node = self.__scene.get_node(self.__node_id)
+            node = self.__scene.get_node(node_id)
             if node:
                 node.update_ui()
             # do callbacks
@@ -248,4 +313,4 @@ class ParameterChangeOp(UndoableOperation):
         self.__scene.add_long_operation(undoop)
 
     def __str__(self):
-        return f'Param Changed {self.__param_name} @ {self.__node_id}'
+        return f'Param Changed {self.__param_name} @ {self.__node_sid}'
