3ab14016cf7008b3469e9c4bd1c6eeb0be98777c
pedohorse <xapkohheh@gmail.com>
2023-04-22 19:59:29 +0200

    added closeable, default size, default geometry

--- src/lifeblood_viewer/ui_elements_base.py
+++ src/lifeblood_viewer/ui_elements_base.py
@@ -1,7 +1,6 @@
 import imgui
-import uuid
 
-from typing import Tuple
+from typing import Optional, Tuple
 
 
 class ImguiElement:
@@ -13,12 +12,16 @@ class ImguiElement:
 
 
 class ImguiWindow(ImguiElement):
-    def __init__(self, title: str = ''):
+    def __init__(self, title: str = '', closable: bool = True):
         self.__opened = False
         self.__just_opened = False
         # wanted to use uuid.uuid4().hex below, but that would bloat imgui's internal db after multiple launches
         self.__imgui_name = f'{title}##{type(self).__name__}'
         self.__focused_last_draw: bool = False
+        self.__closable = closable
+
+    def _imgui_window_name(self):
+        return self.__imgui_name
 
     def popup(self):
         self.__opened = True
@@ -37,8 +40,18 @@ class ImguiWindow(ImguiElement):
         """
         return self.__just_opened
 
-    def default_size(self) -> Tuple[float, float]:
-        return 256, 256
+    def default_size(self) -> Optional[Tuple[float, float]]:
+        """
+        default size on each popup
+        """
+        return None
+
+    def initial_geometry(self) -> Optional[Tuple[int, int, int, int]]:
+        """
+        return initial position and size of the window
+        will be only applied ON FIRST EVER CREATION if returns not None
+        """
+        return None
 
     def draw(self):
         if not self.__opened:
@@ -47,11 +60,15 @@ class ImguiWindow(ImguiElement):
         if self.__just_opened:
             pos = imgui.get_mouse_pos()
             imgui.set_next_window_position(pos.x, pos.y, imgui.APPEARING)
-            size = self.default_size()
-            imgui.set_next_window_size(size[0], size[1], imgui.APPEARING)
+            if init_geo := self.initial_geometry():
+                x, y, w, h =init_geo
+                imgui.set_next_window_size(w, h, imgui.FIRST_USE_EVER)
+                imgui.set_next_window_position(x, y, imgui.FIRST_USE_EVER)
+            if size := self.default_size():
+                imgui.set_next_window_size(size[0], size[1], imgui.APPEARING)
             imgui.set_next_window_focus()
 
-        (expanded, opened) = imgui.begin(self.__imgui_name, closable=True)
+        (expanded, opened) = imgui.begin(self.__imgui_name, closable=self.__closable)
         if not opened:
             imgui.end()
             self._close()
@@ -96,10 +113,10 @@ class ImguiPopup(ImguiWindow):
 
     def draw(self):
         if self.__to_be_opened:
-            imgui.open_popup(self.__imgui_name)
+            imgui.open_popup(self._imgui_window_name())
         self.__to_be_opened = False
 
-        if imgui.begin_popup(self.__imgui_name):
+        if imgui.begin_popup(self._imgui_window_name()):
             self.__focused_last_draw = imgui.is_window_focused()
             self.draw_window_elements()
             imgui.end_popup()
