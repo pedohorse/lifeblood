2b71a9b04590e08cf0331f6c7ff1aac770f1086b
pedohorse <13556996+pedohorse@users.noreply.github.com>
2023-07-10 21:33:09 +0200

    added send_retry_attempts argument, wrapping connection opening errors

--- src/lifeblood/net_messages/client.py
+++ src/lifeblood/net_messages/client.py
@@ -16,7 +16,8 @@ class MessageClient:
     def __init__(self, queue: MessageQueue, session: uuid.UUID, *,
                  source_address_chain: AddressChain,
                  destination_address_chain: AddressChain,
-                 message_stream_factory: MessageStreamFactory):
+                 message_stream_factory: MessageStreamFactory,
+                 send_retry_attempts: int = 6):
         self.__message_queue = queue
         self.__last_sent_message: Optional[Message] = None
         self.__session: uuid.UUID = session
@@ -28,14 +29,17 @@ class MessageClient:
         self.__destination = destination_address_chain.split_address()
         self.__destination_str = destination_address_chain
 
-        self.__attempts = 10
+        self.__attempts = send_retry_attempts
         self.__init_timeout = 1
 
     def session(self) -> uuid.UUID:
         return self.__session
 
     async def send_message(self, data: bytes) -> Message:
-        stream = await self.__message_stream_factory.open_sending_stream(self.__destination[0], self.__source[0])
+        try:
+            stream = await self.__message_stream_factory.open_sending_stream(self.__destination[0], self.__source[0])
+        except Exception as e:
+            raise MessageSendingError('failed to open connection', wrapped_exception=e) from None
 
         last_exception = None
         timeout = self.__init_timeout
@@ -58,6 +62,7 @@ class MessageClient:
                 await asyncio.sleep(timeout)
                 timeout *= 2
         else:
+            self.logger.error(f'unable to sent message, out of attempts, failing.')
             raise last_exception
 
     async def receive_message(self) -> Message:
