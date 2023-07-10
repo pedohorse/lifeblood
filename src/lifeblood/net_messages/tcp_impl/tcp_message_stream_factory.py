12f816d7fec1257849d1822e37ab54e0b51189a3
pedohorse <13556996+pedohorse@users.noreply.github.com>
2023-07-10 21:33:09 +0200

    connection pool implemented better

--- src/lifeblood/net_messages/tcp_impl/tcp_message_stream_factory.py
+++ src/lifeblood/net_messages/tcp_impl/tcp_message_stream_factory.py
@@ -1,7 +1,10 @@
 import asyncio
 import struct
+import logging
+from lifeblood.logging import get_logger
 from datetime import datetime
 from dataclasses import dataclass
+from ..exceptions import MessageSendingError
 from ..interfaces import MessageStreamFactory
 from ..stream_wrappers import MessageSendStream, MessageSendStreamBase
 from ..address import DirectAddress, AddressChain
@@ -25,49 +28,88 @@ class ConnectionPoolEntry:
     writer: asyncio.StreamWriter
     last_used: datetime
     users_count: int
+    close_when_user_count_zero: bool = False
+    bad: bool = False
 
 
 class ReusableMessageSendStream(MessageSendStream):
+    """
+
+    """
     def __init__(self, pool_entry: ConnectionPoolEntry, *, reply_address: DirectAddress, destination_address: DirectAddress):
         super().__init__(pool_entry.reader, pool_entry.writer, reply_address=reply_address, destination_address=destination_address)
         self.__pool_entry = pool_entry
         self.__closed = False
         self.__pool_entry.users_count += 1
+        self.__do_actually_wait_closed = False
 
     def close(self):
         if self.__closed:
             return
         self.__closed = True
+        # note: section below might need locking when close() can be called from different threads
+        #  actually, there is much more problems if streams are opened/closed from different threads
         self.__pool_entry.last_used = datetime.now()
         self.__pool_entry.users_count -= 1
+        if self.__pool_entry.users_count == 0 and self.__pool_entry.close_when_user_count_zero:
+            super().close()
+            self.__do_actually_wait_closed = True
 
     async def wait_closed(self):
-        return
+        if not self.__do_actually_wait_closed:
+            return
+        await super().wait_closed()
+
+    def __del__(self):
+        self.close()  # multiple close() calls are fine
 
 
-class TcpMessageStreamPooledFactory:
+class TcpMessageStreamPooledFactory(MessageStreamFactory):
+    _logger: Optional[logging.Logger] = None
+
     def __init__(self, pooled_connection_life: int = 0, connection_open_function: Optional[Callable[[DirectAddress, DirectAddress], Awaitable[Tuple[asyncio.StreamReader, asyncio.StreamWriter]]]] = None):
         self.__pooled_connection_life = pooled_connection_life
         self.__pool: Dict[Tuple[str, int], List[ConnectionPoolEntry]] = {}
         self.__connection_open_func: Callable[[DirectAddress, DirectAddress], Awaitable[Tuple[asyncio.StreamReader, asyncio.StreamWriter]]] = connection_open_function or _initialize_connection
+        self.__open_connection_calls_count = 0
+        self.__pool_closed = asyncio.Event()
+        if self._logger is None:
+            TcpMessageStreamPooledFactory._logger = get_logger('TcpMessageStreamPooledFactory')
 
     async def prune(self):
+        await self.close_unused_connections_older_than(self.__pooled_connection_life)
+
+    async def close_unused_connections_older_than(self, older_than_this_seconds: float):
         keys_to_remove = []
         now = datetime.now()
+        to_wait = []
         for key, entry_list in self.__pool.items():
             new_entries = []
             for entry in entry_list:
-                if entry.users_count > 0 or (now - entry.last_used).total_seconds() < self.__pooled_connection_life:
+                if not entry.bad \
+                        and (entry.users_count > 0
+                             or ((now - entry.last_used).total_seconds() < older_than_this_seconds
+                                 and not entry.close_when_user_count_zero)
+                             ):
                     new_entries.append(entry)
                 else:
-                    entry.writer.close()
-                    await entry.writer.wait_closed()
-                if len(new_entries) == 0:
-                    keys_to_remove.append(key)
-                else:
-                    self.__pool[key] = new_entries
+                    if not entry.writer.is_closing():
+                        entry.writer.close()
+                    to_wait.append(asyncio.create_task(entry.writer.wait_closed()))
+            if len(new_entries) == 0:
+                keys_to_remove.append(key)
+            else:
+                self.__pool[key] = new_entries
         for key in keys_to_remove:
             self.__pool.pop(key)
+
+        if to_wait:
+            dones, _ = await asyncio.wait(to_wait, return_when=asyncio.ALL_COMPLETED)
+            for task in dones:
+                exc = task.exception()
+                if exc is None:
+                    continue
+                self._logger.warning(f'pooled connection was faulty and failed to close: "{exc}"')
 
     def _get_cached_entry(self, host: str, port: int) -> Optional[ConnectionPoolEntry]:
         key = (host, port)
@@ -78,9 +120,9 @@ class TcpMessageStreamPooledFactory:
         to_remove = []
         selected: Optional[ConnectionPoolEntry] = None
         for entry in entry_list:
-            if entry.users_count > 0:
+            if entry.users_count > 0 or entry.close_when_user_count_zero:
                 continue
-            if entry.reader.at_eof() or entry.writer.is_closing():
+            if entry.bad or entry.reader.at_eof() or entry.writer.is_closing():
                 to_remove.append(entry)
                 continue
             selected = entry
@@ -89,12 +131,24 @@ class TcpMessageStreamPooledFactory:
         return selected
 
     async def open_sending_stream(self, destination: DirectAddress, source: DirectAddress) -> MessageSendStreamBase:
+        if self.__pool_closed.is_set():
+            raise RuntimeError('cannot open sending stream when pool closed')
         host, sport = destination.split(':')
         port = int(sport)
         entry = self._get_cached_entry(host, port)
+        if entry is not None:  # test entry with ping message
+            stream = ReusableMessageSendStream(entry, reply_address=source, destination_address=destination)
+            try:
+                await stream.send_ping()
+            except MessageSendingError as e:
+                self._logger.debug('ping failed due to %s', e)
+                entry.bad = True
+                entry = None
+
         if entry is None:
             key = (host, port)
             reader, writer = await self.__connection_open_func(destination, source)
+            self.__open_connection_calls_count += 1
             entry = ConnectionPoolEntry(reader,
                                         writer,
                                         datetime.now(),
@@ -108,6 +162,26 @@ class TcpMessageStreamPooledFactory:
         stream = ReusableMessageSendStream(entry, reply_address=source, destination_address=destination)
         await self.prune()
         return stream
+
+    async def close_all_unused_connections(self):
+        await self.close_unused_connections_older_than(-1)
+
+    def close_pool(self):
+        self.__pool_closed.set()
+        for items in self.__pool.values():
+            for item in items:
+                item.close_when_user_count_zero = True
+
+    async def wait_pool_closed(self):
+        await self.__pool_closed.wait()
+        while len(self.__pool) > 0:
+            await self.close_all_unused_connections()
+            await asyncio.sleep(0.5)  # too heavy polling?
+
+    def __del__(self):
+        self._logger.debug(f'connections opened total: {self.__open_connection_calls_count}')
+        if len(self.__pool) > 0:
+            self._logger.warning(f'pooled connections remain ({len(self.__pool)}) after __del__, {len([1 for l in self.__pool.values() for x in l if x.users_count > 0])} still in use')
 
 
 class TcpMessageStreamFactory(MessageStreamFactory):
