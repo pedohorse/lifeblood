14d96f973cf1b6d5aaeedf3ffefa6ef6bdaf7480
pedohorse <13556996+pedohorse@users.noreply.github.com>
2023-07-10 21:33:09 +0200

    net messages reworked, tests adjusted

--- src/lifeblood/net_messages.py
+++ src/lifeblood/net_messages.py
@@ -1,67 +1,199 @@
 import asyncio
 import struct
+import uuid
 from io import BytesIO
 
-from typing import Optional
+from typing import Optional, Tuple, Union
 
 
-class WriterMessageWrapper:
+class MessageInterface:
+    def message_size(self) -> int:
+        raise NotImplementedError()
+
+    def message_body(self) -> Union[bytes, memoryview]:
+        raise NotImplementedError()
+
+    def message_destination(self) -> str:
+        raise NotImplementedError()
+
+    def message_session(self) -> Optional[uuid.UUID]:
+        raise NotImplementedError()
+
+
+class Message(MessageInterface):
+    def __init__(self, data: bytes, destination: str, session: Optional[uuid.UUID]):
+        self.__data = data
+        self.__destination = destination
+        self.__session = session
+
+    @classmethod
+    def _write_header(cls, stream: asyncio.StreamWriter, data_size: int, destination: str, session: Optional[uuid.UUID]):
+        dest_data = destination.encode('UTF-8')
+        dest_data_size = len(dest_data)
+        has_session = session is not None
+        stream.write(struct.pack('>QQ?', data_size + 8 + dest_data_size + 1 + (16 if has_session else 0), dest_data_size, session is not None))
+        stream.write(dest_data)
+        if has_session:
+            stream.write(session.bytes)
+
+    @classmethod
+    async def _read_header(cls, stream: asyncio.StreamReader) -> Tuple[int, str, Optional[uuid.UUID]]:
+        """
+        returns: (data_size, destination, session)
+        """
+        message_size, dest_data_size, has_session = struct.unpack('>QQ?', await stream.readexactly(17))
+        destination = (await stream.readexactly(dest_data_size)).decode('UTF-8')
+        session = uuid.UUID(bytes=await stream.readexactly(16)) if has_session else None
+        return message_size - 8 - dest_data_size - 1 - (16 if has_session else 0),\
+               destination,\
+               session
+
+    @classmethod
+    async def from_stream_reader(cls, stream: asyncio.StreamReader):
+        data_size, destination, session = await cls._read_header(stream)
+        data = await stream.readexactly(data_size)
+        return Message(data, destination, session)
+
+    async def serialize_to_stream_writer(self, stream: asyncio.StreamWriter):
+        self._write_header(stream, len(self.__data), self.__destination, self.__session)
+        stream.write(self.__data)
+        await stream.drain()
+
+    def message_size(self) -> int:
+        return len(self.__data)
+
+    def message_body(self) -> Union[bytes, memoryview]:
+        return self.__data
+
+    def message_destination(self) -> str:
+        return self.__destination
+
+    def message_session(self) -> Optional[uuid.UUID]:
+        return self.__session
+
+
+class WriterStreamMessageWrapper(MessageInterface):
     """
     wraps writes into "messages" that have predictable borders, that can be redirected easily
     """
-    def __init__(self, writer: asyncio.StreamWriter, expect_answer=False):
+    def __init__(self, writer: asyncio.StreamWriter, *, destination: str, session: Optional[uuid.UUID] = None):
         self.__writer = writer
         self.__buffer: Optional[BytesIO] = None
         self.__size = 0
-        self.__expect_answer = expect_answer
+        self.__destination = destination
+        self.__session = session
+        self.__initialized = False
 
-    def set_expect_answer(self, expect_answer):
-        self.__expect_answer = expect_answer
-
-    async def __aenter__(self) -> "WriterMessageWrapper":
+    async def __aenter__(self) -> "WriterStreamMessageWrapper":
+        if self.__initialized:
+            raise RuntimeError('message was already initialized')
+        self.__initialized = True
         self.__buffer = BytesIO()
         self.__size = 0
         return self
 
     def write(self, data: bytes):
+        if not self.__initialized:
+            raise RuntimeError('message was not initialized')
         self.__size += len(data)
         self.__buffer.write(data)
 
     def write_string(self, s: str):
+        if not self.__initialized:
+            raise RuntimeError('message was not initialized')
         b = s.encode('UTF-8')
         self.write(struct.pack('>Q', len(b)))
         self.write(b)
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        self.__writer.write(struct.pack('>Q?', self.__size, self.__expect_answer))
+        Message._write_header(self.__writer, self.__size, self.__destination, self.__session)
         self.__writer.write(self.__buffer.getbuffer())
         await self.__writer.drain()
 
+    def message_size(self) -> int:
+        if not self.__initialized:
+            raise RuntimeError('message was not initialized')
+        return self.__size
 
-class ReaderMessageWrapper:
+    def message_body(self) -> Union[bytes, memoryview]:
+        if not self.__initialized:
+            raise RuntimeError('message was not initialized')
+        return self.__buffer.getbuffer()
+
+    def message_destination(self) -> str:
+        if not self.__initialized:
+            raise RuntimeError('message was not initialized')
+        return self.__destination
+
+    def message_session(self) -> Optional[uuid.UUID]:
+        if not self.__initialized:
+            raise RuntimeError('message was not initialized')
+        return self.__session
+
+
+class ReaderStreamMessageWrapper(MessageInterface):
     def __init__(self, reader: asyncio.StreamReader):
         self.__reader = reader
-        self.__message_size: Optional[int] = None
-        self.__expects_answer: Optional[bool] = None
+        self.__message_body_size: int = 0
         self.__already_read = 0
+        self.__destination: Optional[str] = None
+        self.__initialized = False
+        self.__buffer = BytesIO()
+        self.__session = None
 
     def message_size(self):
-        return self.__message_size
+        if not self.__initialized:
+            raise RuntimeError('message was not initialized')
+        return self.__message_body_size
 
-    def is_answer_expected(self):
-        return self.__expects_answer
+    def message_body(self) -> Union[bytes, memoryview]:
+        """
+        message body so far. this ONLY guaranteed returns the whole message body after with block end
+        """
+        if not self.__initialized:
+            raise RuntimeError('message was not initialized')
+        return self.__buffer.getbuffer()
 
-    async def __aenter__(self) -> "ReaderMessageWrapper":
-        self.__message_size, self.__expects_answer = struct.unpack('>Q?', await self.__reader.readexactly(9))
+    def message_destination(self) -> str:
+        if not self.__initialized:
+            raise RuntimeError('message was not initialized')
+        return self.__destination
+
+    def message_session(self) -> Optional[uuid.UUID]:
+        if not self.__initialized:
+            raise RuntimeError('message was not initialized')
+        return self.__session
+
+    async def __aenter__(self) -> "ReaderStreamMessageWrapper":
+        if self.__initialized:
+            raise RuntimeError('message was already initialized')
+        self.__initialized = True
+        self.__message_body_size, self.__destination, self.__session = await Message._read_header(self.__reader)
         self.__already_read = 0
         return self
 
     async def readexactly(self, size: int):
+        if size <= 0:
+            return
         self.__already_read += size
-        return await self.__reader.readexactly(size)
-
-    async def readall(self):
-        return await self.__reader.readexactly(self.__message_size - self.__already_read)
+        data = await self.__reader.readexactly(size)
+        self.__buffer.write(data)
+        return data
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.readexactly(self.__message_body_size - self.__already_read)
+
+
+class MessageStream:
+    def __init__(self, reader_stream: asyncio.StreamReader, writer_stream: asyncio.StreamWriter):
+        self.__reader = reader_stream
+        self.__writer = writer_stream
+        self.__current_session: Optional[uuid.UUID] = None
+
+    def new_message(self, destination: str, force_new_session=True) -> WriterStreamMessageWrapper:
+        if self.__current_session is None or force_new_session:
+            self.__current_session = uuid.uuid4()
+        return WriterStreamMessageWrapper(self.__writer, destination=destination, session=self.__current_session)
+
+    def start_receiving_message(self):
         pass
--- tests/test_net_messages.py
+++ tests/test_net_messages.py
@@ -2,7 +2,8 @@ import asyncio
 import struct
 import random
 import string
-from lifeblood.net_messages import ReaderMessageWrapper, WriterMessageWrapper
+import uuid
+from lifeblood.net_messages import ReaderStreamMessageWrapper, WriterStreamMessageWrapper, Message
 from unittest import IsolatedAsyncioTestCase
 from io import BufferedReader, BufferedWriter, BytesIO
 
@@ -16,6 +17,9 @@ class DummyStream:
 
     def write(self, data: bytes):
         self.__buffer.write(data)
+
+    def rewind(self):
+        self.__buffer.seek(0)
 
     async def readexactly(self, size: int):
         await asyncio.sleep(0)
@@ -31,50 +35,61 @@ class DummyStream:
 class TestNetMessages(IsolatedAsyncioTestCase):
     async def test_simple_writer(self):
         stream = DummyStream()
-        async with WriterMessageWrapper(stream) as writer:
+        async with WriterStreamMessageWrapper(stream, destination='foo;бар') as writer:
             writer.write(b'test')
 
         buffer = stream.get_buffer()
-        self.assertEqual(struct.pack('>Q?', 4, False) + b'test', buffer.getbuffer())
+        self.assertEqual(struct.pack('>QQ?', 23, 10, False) + b'foo;\xd0\xb1\xd0\xb0\xd1\x80' + b'test', bytes(buffer.getbuffer()))
 
     async def test_empty_writer(self):
         stream = DummyStream()
-        async with WriterMessageWrapper(stream, expect_answer=True) as writer:
+        async with WriterStreamMessageWrapper(stream, destination='bee;boo') as writer:
             writer.write(b'')
 
         buffer = stream.get_buffer()
-        self.assertEqual(struct.pack('>Q?', 0, True), buffer.getbuffer())
+        self.assertEqual(struct.pack('>QQ?', 16, 7, False) + b'bee;boo', bytes(buffer.getbuffer()))
 
     async def test_random_writer(self):
         rng = random.Random(666666)
         for _ in range(666):
             data = rng.randbytes(rng.randint(0, 12345))
-            reply = rng.random() > 0.5
             stream = DummyStream()
-            async with WriterMessageWrapper(stream, expect_answer=reply) as writer:
+            dest = ''.join(rng.choice(string.ascii_letters) for _ in range(rng.randint(0, 100)))
+            session = uuid.UUID(bytes=rng.randbytes(16)) if rng.random() > 0.5 else None
+            async with WriterStreamMessageWrapper(stream, destination=dest, session=session) as writer:
                 writer.write(data)
 
-            buffer = stream.get_buffer()
-            self.assertEqual(struct.pack('>Q?', len(data), reply) + data, buffer.getbuffer())
+            stream.rewind()
+
+            msg = await Message.from_stream_reader(stream)
+            self.assertEqual(len(data), msg.message_size())
+            self.assertEqual(data, msg.message_body())
+            self.assertEqual(session, msg.message_session())
+            self.assertEqual(dest, msg.message_destination())
 
     async def test_simple_reader(self):
-        stream = DummyStream(BytesIO(struct.pack('>Q?', 10, True) + b'doubletest'))
-        async with ReaderMessageWrapper(stream) as reader:
+        stream = DummyStream(BytesIO(struct.pack('>QQ?', 24, 5, False) + b'fooba' + b'doubletest'))
+        async with ReaderStreamMessageWrapper(stream) as reader:
             data = await reader.readexactly(10)
 
         self.assertEqual(b'doubletest', data)
         self.assertEqual(10, reader.message_size())
-        self.assertEqual(True, reader.is_answer_expected())
+        self.assertEqual('fooba', reader.message_destination())
 
     async def test_random_reader(self):
         rng = random.Random(666667)
         for _ in range(666):
             exp_data = rng.randbytes(rng.randint(0, 12345))
-            reply = rng.random() > 0.5
-            stream = DummyStream(BytesIO(struct.pack('>Q?', len(exp_data), reply) + exp_data))
-            async with ReaderMessageWrapper(stream) as reader:
-                data = await reader.readall()
+            dest = ''.join(rng.choice(string.ascii_letters) for _ in range(rng.randint(0, 100)))
+            dest_data = dest.encode('UTF-8')
+            session = uuid.UUID(bytes=rng.randbytes(16)) if rng.random() > 0.5 else None
+            stream = DummyStream(BytesIO(struct.pack('>QQ?', len(exp_data) + 8 + len(dest_data) + 1 + (16 if session else 0), len(dest_data), session is not None) + dest_data + (session.bytes if session else b'') + exp_data))
+            async with ReaderStreamMessageWrapper(stream) as reader:
+                data_read = await reader.readexactly(min(11, len(exp_data)))
+            data_all = reader.message_body()
 
-            self.assertEqual(exp_data, data)
+            self.assertEqual(exp_data, data_all)
+            self.assertEqual(exp_data[:11], data_read)
             self.assertEqual(len(exp_data), reader.message_size())
-            self.assertEqual(reply, reader.is_answer_expected())
+            self.assertEqual(dest, reader.message_destination())
+            self.assertEqual(session, reader.message_session())
