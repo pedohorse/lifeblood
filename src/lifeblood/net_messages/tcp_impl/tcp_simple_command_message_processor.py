f3d39f70cbf5ea5d7c2e045b28ca132d1300c738
pedohorse <13556996+pedohorse@users.noreply.github.com>
2023-08-05 01:41:07 +0200

    better error handling

--- src/lifeblood/net_messages/tcp_impl/tcp_simple_command_message_processor.py
+++ src/lifeblood/net_messages/tcp_impl/tcp_simple_command_message_processor.py
@@ -18,6 +18,7 @@ from typing import Awaitable, Callable, Dict, Optional, Tuple
 class JsonMessageWrapper(Message):
     def __init__(self, message: Message):
         super().__init__(b'', MessageType.DEFAULT_MESSAGE, AddressChain(''), AddressChain(''), None)
+        self.copy_from(message)
         self.__wrapped_message = message
         self.__body_as_json = None
 
@@ -27,7 +28,7 @@ class JsonMessageWrapper(Message):
     async def message_body_as_json(self) -> dict:
         if self.__body_as_json is None:
             self.__body_as_json = await asyncio.get_event_loop().run_in_executor(None,
-                                                                                 lambda s: json.loads(s.decode('utf-8')),
+                                                                                 lambda s: json.loads(bytes(s).decode('utf-8')),
                                                                                  self.message_body())
         return self.__body_as_json
 
@@ -52,7 +53,7 @@ class JsonMessageClientFactory(MessageClientFactory):
                               source_address_chain: AddressChain,
                               destination_address_chain: AddressChain,
                               message_stream_factory: MessageStreamFactory,
-                              send_retry_attempts: int = 6) -> MessageClient:
+                              send_retry_attempts: int = 2) -> MessageClient:
         return JsonMessageClient(queue, session,
                                  source_address_chain=source_address_chain,
                                  destination_address_chain=destination_address_chain,
@@ -90,6 +91,7 @@ class TcpJsonMessageProcessor(MessageProcessorBase):
         try:
             await jmessage.message_body_as_json()
         except (json.JSONDecodeError, UnicodeDecodeError):
+            self._logger.exception('failed to parse json')
             return await self.process_unknown_message(message, client)
         return await self.process_json_message(jmessage, client)
 
@@ -97,7 +99,7 @@ class TcpJsonMessageProcessor(MessageProcessorBase):
         raise NotImplementedError()
 
     async def process_unknown_message(self, message: Message, client: MessageClient):
-        self._logger.warning('unexpected non-json message, ignoring')
+        self._logger.warning(f'unexpected non-json message, ignoring {bytes(message.message_body()[:128])}')
 
 
 class CommandJsonMessageClient(JsonMessageClient):
@@ -115,7 +117,7 @@ class CommandJsonMessageClientFactory(JsonMessageClientFactory):
                               source_address_chain: AddressChain,
                               destination_address_chain: AddressChain,
                               message_stream_factory: MessageStreamFactory,
-                              send_retry_attempts: int = 6) -> MessageClient:
+                              send_retry_attempts: int = 2) -> MessageClient:
         return CommandJsonMessageClient(queue, session,
                                         source_address_chain=source_address_chain,
                                         destination_address_chain=destination_address_chain,
@@ -131,23 +133,28 @@ class TcpCommandMessageProcessor(TcpJsonMessageProcessor):
                          connection_pool_cache_time=connection_pool_cache_time,
                          message_client_factory=CommandJsonMessageClientFactory())
 
-    def command_mapping(self) -> Dict[str, Callable[[dict, CommandJsonMessageClient], Awaitable[None]]]:
+    def command_mapping(self) -> Dict[str, Callable[[dict, CommandJsonMessageClient, Message], Awaitable[None]]]:
         raise NotImplementedError()
 
     async def process_json_message(self, message: JsonMessageWrapper, client: CommandJsonMessageClient):
-        if 'command' in message:
-            command_json = message['command']
+        if not await self.should_process(message):
+            self._logger.debug(f'should not process, skipping message {message}')
+            return
+
+        body = await message.message_body_as_json()
+        if 'command' in body:
+            command_json = body['command']
             command = command_json['name']
             args = command_json.get('arguments', {})
 
             commands_map = self.command_mapping()
             if command not in commands_map:
-                await self.process_unknown_command(command, args, client)
+                await self.process_unknown_command(command, args, client, message)
                 return
 
-            await commands_map[command](args, client)
+            await commands_map[command](args, client, message)
         else:
             self._logger.error(f'unexpected message to the worker, "{message.message_body()}"')
 
-    async def process_unknown_command(self, command: str, args: dict, client: CommandJsonMessageClient):
+    async def process_unknown_command(self, command: str, args: dict, client: CommandJsonMessageClient, original_message: JsonMessageWrapper):
         self._logger.error(f'unknown command {command}')
