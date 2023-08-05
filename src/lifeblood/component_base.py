02c6a69df25009bfd33a53a5e6d672b61cd04052
pedohorse <13556996+pedohorse@users.noreply.github.com>
2023-08-05 16:18:33 +0200

    explicitly create waiter task, as other way is deprecated

--- src/lifeblood/component_base.py
+++ src/lifeblood/component_base.py
@@ -23,7 +23,8 @@ class ComponentBase:
         if self.__main_task is not None:
             raise RuntimeError('already started')
         self.__main_task = asyncio.create_task(self._main_task())
-        done, others = await asyncio.wait([self.__main_task_is_ready.wait(), self.__main_task], return_when=asyncio.FIRST_COMPLETED)
+        ready_waiter = asyncio.create_task(self.__main_task_is_ready.wait())
+        done, others = await asyncio.wait([ready_waiter, self.__main_task], return_when=asyncio.FIRST_COMPLETED)
         if self.__main_task in done:  # means it raised an error
             for other in others:
                 other.cancel()
