c99aad270a4fad2c1d62a2658683fd37bd98491d
pedohorse <13556996+pedohorse@users.noreply.github.com>
2023-08-23 02:07:07 +0200

    tests for task/invocation messaging

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -2,6 +2,7 @@ import asyncio
 from unittest import IsolatedAsyncioTestCase
 from pathlib import Path
 import sqlite3
+from lifeblood.enums import InvocationState, TaskState
 from lifeblood.db_misc import sql_init_script
 from lifeblood.scheduler.scheduler import Scheduler
 from lifeblood.scheduler_message_processor import SchedulerWorkerControlClient
@@ -34,7 +35,7 @@ class SchedulerTests(IsolatedAsyncioTestCase):
 
     async def test_stopping_normal(self):
         purge_db()
-        sched = Scheduler('test_swc.db', do_broadcasting=False, helpers_minimal_idle_to_ensure=0)
+        sched = Scheduler('test_swc1.db', do_broadcasting=False, helpers_minimal_idle_to_ensure=0)
         await sched.start()
         # crudely assert that there are corutines running from scheduler
         self.assertTrue(any([Path(x.get_coro().cr_code.co_filename).parts[-3:-1] == ('lifeblood', 'scheduler') for x in asyncio.all_tasks()]))
@@ -53,7 +54,7 @@ class SchedulerTests(IsolatedAsyncioTestCase):
         tests that scheduler stops even without call to wait_till_stops
         """
         purge_db()
-        sched = Scheduler('test_swc.db', do_broadcasting=False, helpers_minimal_idle_to_ensure=0)
+        sched = Scheduler('test_swc1.db', do_broadcasting=False, helpers_minimal_idle_to_ensure=0)
         await sched.start()
         # crudely assert that there are corutines running from scheduler
         self.assertTrue(any([Path(x.get_coro().cr_code.co_filename).parts[-3:-1] == ('lifeblood', 'scheduler') for x in asyncio.all_tasks()]))
@@ -96,7 +97,7 @@ class SchedulerTests(IsolatedAsyncioTestCase):
         and it is expected to fail to be opened
         """
         purge_db()
-        sched = Scheduler('test_swc.db', do_broadcasting=False, helpers_minimal_idle_to_ensure=0, server_addr=('127.0.0.1', 11847, 11848))
+        sched = Scheduler('test_swc1.db', do_broadcasting=False, helpers_minimal_idle_to_ensure=0, server_addr=('127.0.0.1', 11847, 11848))
         await sched.start()
         proc = TcpJsonMessageProcessor(('127.0.0.1', 11850))
         await proc.start()
@@ -142,7 +143,7 @@ class SchedulerTests(IsolatedAsyncioTestCase):
         and it is expected to fail to be opened
         """
         purge_db()
-        sched = Scheduler('test_swc.db', do_broadcasting=False, helpers_minimal_idle_to_ensure=0, server_addr=('127.0.0.1', 11847, 11848))
+        sched = Scheduler('test_swc1.db', do_broadcasting=False, helpers_minimal_idle_to_ensure=0, server_addr=('127.0.0.1', 11847, 11848))
         await sched.start()
 
         async with SchedulerTaskClient('127.0.0.1', 11847) as client:
@@ -177,3 +178,33 @@ class SchedulerTests(IsolatedAsyncioTestCase):
         await asyncio.wait([waiter_task], timeout=5)
         self.assertFalse(any([Path(x.get_coro().cr_code.co_filename).parts[-3:-1] == ('lifeblood', 'scheduler') for x in asyncio.all_tasks()]))
         self.assertFalse(any([Path(x.get_coro().cr_code.co_filename).parts[-2:-1] == ('lifeblood',) for x in asyncio.all_tasks()]))
+
+    async def test_get_invocation_workers(self):
+        purge_db()
+
+        sched = Scheduler('test_swc1.db', do_broadcasting=False, helpers_minimal_idle_to_ensure=0)
+        await sched.start()
+
+        with sqlite3.connect('test_swc1.db') as con:
+            con.execute('INSERT INTO nodes (type, name, node_object) VALUES ("null", "test", NULL)')
+            con.execute('INSERT INTO tasks (node_id, name, state) VALUES (1, "foo", ?)', (TaskState.IN_PROGRESS.value,))
+            con.execute('INSERT INTO workers (last_address, ping_state, state, hwid) VALUES ("1.2.3.4:5678", 1, 2, 123456)')
+            con.execute('INSERT INTO workers (last_address, ping_state, state, hwid) VALUES ("2.3.4.5:6789", 1, 2, 123457)')
+            con.execute('INSERT INTO workers (last_address, ping_state, state, hwid) VALUES ("3.4.5.6:7890", 1, 2, 123457)')
+            con.execute('INSERT INTO invocations (task_id, worker_id, node_id, state) VALUES (1, 2, 1, ?)', (InvocationState.IN_PROGRESS.value,))
+            con.execute('INSERT INTO invocations (task_id, worker_id, node_id, state) VALUES (1, 3, 1, ?)', (InvocationState.FINISHED.value,))
+            con.execute('INSERT INTO invocations (task_id, worker_id, node_id, state) VALUES (1, 1, 1, ?)', (InvocationState.INVOKING.value,))
+            con.commit()
+        try:
+
+            self.assertEqual(InvocationState.IN_PROGRESS, await sched.get_invocation_state(1))
+            self.assertEqual(InvocationState.FINISHED, await sched.get_invocation_state(2))
+            self.assertEqual(InvocationState.INVOKING, await sched.get_invocation_state(3))
+
+            self.assertEqual("2.3.4.5:6789", await sched.get_invocation_worker(1))
+            self.assertEqual("3.4.5.6:7890", await sched.get_invocation_worker(2))
+            self.assertEqual("1.2.3.4:5678", await sched.get_invocation_worker(3))
+        finally:
+            sched.stop()
+            await sched.wait_till_stops()
+            
--- tests/test_scheduler_worker_comm.py
+++ tests/test_scheduler_worker_comm.py
@@ -15,14 +15,14 @@ from lifeblood.worker import Worker
 from lifeblood.invocationjob import InvocationJob
 from lifeblood.scheduler import Scheduler
 from lifeblood.taskspawn import NewTask
-from lifeblood.enums import WorkerType, WorkerState, SpawnStatus
+from lifeblood.enums import WorkerType, WorkerState, SpawnStatus, InvocationState
 from lifeblood.db_misc import sql_init_script
 from lifeblood.logging import set_default_loglevel
 from lifeblood.config import get_config
 from lifeblood.nethelpers import get_default_addr
 from lifeblood import launch
 
-from typing import Awaitable, Callable
+from typing import Awaitable, Callable, List, Optional, Tuple
 
 
 def purge_db():
@@ -66,7 +66,7 @@ class SchedulerWorkerCommSameProcess(IsolatedAsyncioTestCase):
                              worker.wait_till_stops())
 
     async def test_worker_invocation_api1(self):
-        async def _logic(scheduler, worker, tmp_script_path, done_waiter):
+        async def _logic(scheduler, workers, tmp_script_path, done_waiter):
             with mock.patch('lifeblood.scheduler.scheduler.Scheduler.spawn_tasks') as spawn_patch:
                 spawn_patch.side_effect = lambda *args, **kwargs: print(f'spawn_tasks_called with {args}, {kwargs}') \
                                                                   or (SpawnStatus.SUCCEEDED, 2346)
@@ -76,7 +76,7 @@ class SchedulerWorkerCommSameProcess(IsolatedAsyncioTestCase):
                         invocation_id=1234,
                     )
                 ij._set_task_id(2345)
-                await worker.run_task(
+                await workers[0].run_task(
                     ij,
                     scheduler.server_message_address()
                 )
@@ -93,7 +93,7 @@ class SchedulerWorkerCommSameProcess(IsolatedAsyncioTestCase):
         )
 
     async def test_worker_invocation_api2(self):
-        async def _logic(scheduler, worker, tmp_script_path, done_waiter):
+        async def _logic(scheduler, workers, tmp_script_path, done_waiter):
             with mock.patch('lifeblood.scheduler.scheduler.Scheduler.update_task_attributes') as attr_patch:
                 attr_patch.side_effect = lambda *args, **kwargs: print(f'update attrs with {args}, {kwargs}')
 
@@ -102,7 +102,7 @@ class SchedulerWorkerCommSameProcess(IsolatedAsyncioTestCase):
                         invocation_id=1234,
                     )
                 ij._set_task_id(2345)
-                await worker.run_task(
+                await workers[0].run_task(
                     ij,
                     scheduler.server_message_address()
                 )
@@ -119,16 +119,85 @@ class SchedulerWorkerCommSameProcess(IsolatedAsyncioTestCase):
             _logic
         )
 
-    async def _helper_test_worker_invocation_api(self, runcode: str, logic: Callable):
+    async def test_worker_invocation_comm_api(self):
+        await self._helper_test_worker_invocation_comm_api()
+
+    async def test_worker_invocation_comm_api_send_delay(self):
+        await self._helper_test_worker_invocation_comm_api(send_delay=5)
+
+    async def test_worker_invocation_comm_api_recv_delay(self):
+        await self._helper_test_worker_invocation_comm_api(recv_delay=5)
+
+    async def _helper_test_worker_invocation_comm_api(self, *,
+                                                      send_delay=0, recv_delay=0):
+        async def _logic(scheduler, workers: List[Worker], tmp_script_path, done_waiter):
+            with mock.patch('lifeblood.scheduler.scheduler.Scheduler.update_task_attributes') as attr_patch, \
+                 mock.patch('lifeblood.scheduler.scheduler.Scheduler.get_invocation_state') as get_invoc_patch, \
+                 mock.patch('lifeblood.scheduler.scheduler.Scheduler.get_invocation_worker') as get_invoc_worker_patch:
+                attr_patch.side_effect = lambda *args, **kwargs: print(f'update attrs with {args}, {kwargs}')
+                get_invoc_patch.return_value = InvocationState.IN_PROGRESS
+                get_invoc_worker_patch.side_effect = lambda inv_id: \
+                    {
+                        11234: workers[0].message_processor().listening_address(),
+                        11235: workers[1].message_processor().listening_address()
+                    }[inv_id]
+
+                ij1 = InvocationJob(
+                        ['python', '-c',
+                            f'import lifeblood_connection as lbc\n'
+                            f'import time\n'
+                            f'time.sleep({send_delay})\n'
+                            f'lbc.message_to_invocation_send(11235, "foobaaaar", b"IamDATAbanana")\n'
+                            f'print("done1")\n'
+                         ],
+                        invocation_id=11234,
+                    )
+                ij1._set_task_id(3456)
+
+                ij2 = InvocationJob(
+                    ['python', '-c',
+                        f'import lifeblood_connection as lbc\n'
+                        f'import time\n'
+                        f'time.sleep({recv_delay})\n'
+                        f'src_inv, data = lbc.message_to_invocation_receive("foobaaaar")\n'
+                        f'assert data == b"IamDATAbanana", data\n'
+                        f'assert src_inv == 11234, src_inv\n'
+                        f'print("done2")\n'
+                     ],
+                    invocation_id=11235,
+                )
+                ij2._set_task_id(3457)
+
+                await workers[0].run_task(
+                    ij1,
+                    scheduler.server_message_address()
+                )
+                await workers[1].run_task(
+                    ij2,
+                    scheduler.server_message_address()
+                )
+
+                await asyncio.wait([done_waiter], timeout=10)
+
+        await self._helper_test_worker_invocation_api(
+            '',
+            _logic,
+            worker_count=2
+        )
+
+    async def _helper_test_worker_invocation_api(self, runcode: str, logic: Callable, *, worker_count: int = 1, tasks_to_complete=None):
         purge_db()
         sched = Scheduler('test_swc.db', do_broadcasting=False, helpers_minimal_idle_to_ensure=0)
         await sched.start()
 
-        worker = Worker(sched.server_message_address())
-        await worker.start()
+        workers = []
+        for i in range(worker_count):
+            worker = Worker(sched.server_message_address())
+            await worker.start()
+            workers.append(worker)
 
         await asyncio.gather(sched.wait_till_starts(),
-                             worker.wait_till_starts())
+                             *[worker.wait_till_starts() for worker in workers])
 
         #
         fd, tmp_script_path = tempfile.mkstemp('.py')
@@ -137,18 +206,31 @@ class SchedulerWorkerCommSameProcess(IsolatedAsyncioTestCase):
                 f.write(runcode)
 
             done_ev = asyncio.Event()
+            tasks_to_complete = tasks_to_complete or worker_count
+            side_effect_was_good = True
             with mock.patch('lifeblood.scheduler.scheduler.Scheduler.task_done_reported') as td_patch:
-                td_patch.side_effect = lambda *args, **kwargs: done_ev.set()
+                def _side_effect(task: InvocationJob, stdout: str, stderr: str):
+                    nonlocal tasks_to_complete, side_effect_was_good
+                    tasks_to_complete -= 1
+                    print(f'finished {task.task_id()} out: {stdout}')
+                    print(f'finished {task.task_id()} err: {stderr}')
+                    side_effect_was_good = side_effect_was_good and 0 == task.exit_code()
+                    if tasks_to_complete <= 0:
+                        done_ev.set()
+
+                td_patch.side_effect = _side_effect
                 done_waiter = asyncio.create_task(done_ev.wait())
-                await logic(sched, worker, tmp_script_path, done_waiter)
+                await logic(sched, workers, tmp_script_path, done_waiter)
+                self.assertTrue(side_effect_was_good)
         finally:
             os.close(fd)
             os.unlink(tmp_script_path)
 
-        worker.stop()
-        await worker.wait_till_stops()
-        sched.stop()
-        await sched.wait_till_stops()
+            for worker in workers:
+                worker.stop()
+                await worker.wait_till_stops()
+            sched.stop()
+            await sched.wait_till_stops()
 
     async def test_task_get_order(self):
         purge_db()
