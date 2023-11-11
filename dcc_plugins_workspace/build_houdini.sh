249671e789e22e375d29eea7f47b7512f651eeb1
pedohorse <13556996+pedohorse@users.noreply.github.com>
2023-11-11 18:57:02 +0100

    add SO_REUSEPORT fix for win

--- dcc_plugins_workspace/build_houdini.sh
+++ dcc_plugins_workspace/build_houdini.sh
@@ -7,3 +7,8 @@ hotl -l houdini/otls/Driver-lifeblood_submitter-1.0.0.hda ../dcc_plugins/houdini
 rsync -arhv --exclude=__pycache__ --exclude="*.pyc" ../src/lifeblood_client ../dcc_plugins/houdini/python2.7libs
 rsync -arhv --exclude=__pycache__ --exclude="*.pyc" ../src/lifeblood_client ../dcc_plugins/houdini/python3.7libs
 rsync -arhv --exclude=__pycache__ --exclude="*.pyc" ../src/lifeblood_client ../dcc_plugins/houdini/python3.9libs
+
+pushd ../dcc_plugins/houdini
+rm ../houdini.zip
+zip -r ../houdini.zip . --exclude "*__pycache__/" --exclude "*.pyc" --exclude "*.md"
+popd
