67ae0f9895e6df6cd1593efea645d3157e2e049b
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-01-19 15:37:15 +0100

    auto build tools

--- dcc_plugins_workspace/build_blender.sh
+++ dcc_plugins_workspace/build_blender.sh
@@ -10,7 +10,7 @@ rsync -arhv --exclude=__pycache__ --exclude="*.pyc" blender/blender_path $OUT
 rsync -arhv --exclude=__pycache__ --exclude="*.pyc" ../src/lifeblood_client $OUT/blender_path/addons/scripts
 rsync -arhv --exclude=__pycache__ --exclude="*.pyc" ../src/lifeblood_client $OUT/blender_path/addons/lifeblood_plugin
 pushd ../dcc_plugins/blender/blender_path/addons
-zip -r ../../../lifeblood_addon.zip lifeblood_plugin --exclude "*__pycache__/" --exclude "*.pyc"
+zip -r ../../../blender_lifeblood_addon.zip lifeblood_plugin --exclude "*__pycache__/" --exclude "*.pyc"
 popd
 
 popd
