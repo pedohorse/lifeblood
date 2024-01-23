a147faa9238f7f1fa266ac6c19126d01693a367d
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-01-23 23:55:27 +0100

    accepts ::warning:: ::error:: tags

--- src/lifeblood_viewer/widgets/flashy_label.py
+++ src/lifeblood_viewer/widgets/flashy_label.py
@@ -16,6 +16,7 @@ class FlashyLabel(QLabel):
         self.__state = 2
         self.__time = 0
         self.__fade_timer = 0
+        self.__color = (255, 255, 255, 255)
 
     def set_size(self, size):
         font = self.font()
@@ -23,6 +24,17 @@ class FlashyLabel(QLabel):
         self.setFont(font)
 
     def show_label(self, label: str, time: float):
+        self.__color = (255, 255, 255, 255)
+        label = label.strip()
+        warn_prefix = '::warning::'
+        err_prefix = '::error::'
+        if label.startswith(warn_prefix):
+            label = label[len(warn_prefix):].lstrip()
+            self.__color = (247, 142, 4, 255)
+        elif label.startswith(err_prefix):
+            label = label[len(err_prefix):].lstrip()
+            self.__color = (165, 17, 9, 255)
+
         self.__state = 0
         self.__time = time
         self.__fade_timer = time / 2
@@ -30,7 +42,7 @@ class FlashyLabel(QLabel):
         self.__hide_timer.start()
         self.setText(label)
         self.resize(self.sizeHint())
-        self.setStyleSheet('QLabel{color: rgba(255, 255, 255, 255);}')
+        self.setStyleSheet(f'QLabel{{color: rgba({", ".join(str(x) for x in self.__color)});}}')
         self.show()
 
     def __timeout(self):
@@ -40,7 +52,7 @@ class FlashyLabel(QLabel):
             self.__hide_timer.start()
         elif self.__state == 1:
             self.__fade_timer -= 0.1
-            self.setStyleSheet(f'QLabel{{color: rgba(255, 255, 255, {max(0, int(255*self.__fade_timer*2/self.__time))});}}')
+            self.setStyleSheet(f'QLabel{{color: rgba({", ".join(str(x) for x in self.__color[:-1])}, {max(0, int(self.__color[-1]*self.__fade_timer*2/self.__time))});}}')
             if self.__fade_timer <= 0:
                 self.__state = 2
                 self.hide()
