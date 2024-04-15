3ffab86dc9be21eb8a692047162311bbb06d5144
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-04-15 17:55:50 +0200

    remove a random copy of urllib3, use compile.sh instead to always build with freshest

--- stock_tools/telegram_client/compile.sh
+++ stock_tools/telegram_client/compile.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+pushd $(dirname "$0")
+
+if [ -d urllib3 ]; then
+  echo "cleanup"
+  rm -rf urllib3
+fi
+pip download "urllib3>=2,<3"
+unzip urllib3*.whl
+rm urllib3*.whl
+rm -rf urllib3*.dist-info
 zip -r telegram_client.pyz __main__.py telebot.py urllib3/
 mv telegram_client.pyz ../../src/lifeblood/stock_nodes/telegram_client/data/.
 printf "all done\n"
+
+popd
