9bd4b698f3252a38b2793d28c764845a0bd0674b
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-04-17 17:05:02 +0200

    remove fixed and commited lib dependencies

--- stock_tools/matrix_client/compile.sh
+++ stock_tools/matrix_client/compile.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
-zip -r matrixclient.pyz __main__.py matrix_client/ requests/ urllib3/ canonicaljson/
+
+set -e -o pipefail
+
+pushd $(dirname "$0")
+
+libs=("urllib3" "matrix_client" "requests" "charset_normalizer" "idna" "certifi")
+
+for lib in ${libs[@]}; do
+  if [ -d $lib ]; then
+    echo "cleanup $lib"
+    rm -rf $lib
+  fi
+done
+pip download "matrix_client>=0.4,<0.5"
+for lib in ${libs[@]}; do
+  unzip ${lib}*.whl
+  rm ${lib}*.whl
+  rm -rf ${lib}*.dist-info
+  rm -rf test
+done
+
+patch matrix_client/api.py matrix_client_api.py.patch
+
+zip -r matrixclient.pyz __main__.py ${libs[@]}
+
+# sanity check test
+python matrixclient.pyz --help
+
 mv matrixclient.pyz ../../src/lifeblood/stock_nodes/matrixclient/data/.
 cp install_matrix_commander.py ../../src/lifeblood/stock_nodes/matrixclient/data/.
+
+
+
 printf "all done\n"
+
+popd
