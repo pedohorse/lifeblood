01f2737b15b728b7803ad5aa47af9f26c5fca267
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-04-20 11:55:58 +0200

    change default temp invocation log path

--- src/lifeblood/worker.py
+++ src/lifeblood/worker.py
@@ -24,7 +24,7 @@ from .invocationjob import InvocationJob
 from .config import get_config
 from . import environment_resolver
 from .enums import WorkerType, WorkerState, ProcessPriorityAdjustment
-from .paths import config_path
+from .paths import log_path
 from .process_utils import kill_process_tree
 from .misc import event_set_context
 from .net_messages.address import AddressChain, DirectAddress
@@ -59,7 +59,7 @@ class Worker:
         config = get_config('worker')
         self.__logger = logging.get_logger('worker')
         self.log_root_path: str = ''
-        for self.log_root_path in (os.path.expandvars(config.get_option_noasync('worker.logpath', config_path('logs', 'worker'))),
+        for self.log_root_path in (os.path.expandvars(config.get_option_noasync('worker.logpath', log_path('invocations', 'worker'))),
                                    os.path.join(tempfile.gettempdir(), 'lifeblood', 'worker_logs')):
             logs_ok = True
             try:
@@ -76,7 +76,7 @@ class Worker:
             self.__logger.warning(f'could not use location {self.log_root_path} for logs, trying another...')
         else:
             raise RuntimeError('could not initialize logs directory')
-        self.__logger.info(f'using {self.log_root_path} for logs')
+        self.__logger.info(f'using {self.log_root_path} for invocation logs')
 
         self.__status = {}
         self.__scheduler_db_uid: int = 0  # unsigned 64bit int
