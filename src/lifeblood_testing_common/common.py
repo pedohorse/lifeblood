cc73e4ba27804ca79a5b8f2a850efa2ebaed4747
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-07-01 14:42:35 +0200

    allow resource definition overrides for testing

--- src/lifeblood_testing_common/common.py
+++ src/lifeblood_testing_common/common.py
@@ -1,4 +1,5 @@
 from lifeblood.scheduler.scheduler import Scheduler
+from lifeblood.worker_resource_definition import WorkerResourceDefinition
 from lifeblood.basenode_serializer_v2 import NodeSerializerV2
 from lifeblood.pluginloader import PluginNodeDataProvider  # TODO: this must be replaced by a testing mocker
 from lifeblood_testing_common.scheduler_config_provider_default_override import SchedulerConfigProviderOverrides
@@ -21,6 +22,7 @@ def create_default_scheduler(
         server_ui_addr: Optional[Tuple[str, int]] = None,
         node_per_node_config: Optional[Dict[str, Dict[str, Any]]] = None,
         node_global_config: Optional[Dict[str, Dict[str, Any]]] = None,
+        resource_definitions: Optional[Tuple[WorkerResourceDefinition, ...]] = None,
 ) -> Scheduler:
     legacy_addr = None
     message_addr = None
@@ -37,6 +39,7 @@ def create_default_scheduler(
         ui_address=server_ui_addr,
         node_per_node_config=node_per_node_config,
         node_global_config=node_global_config,
+        resource_definitions=resource_definitions,
     )
     return Scheduler(
         scheduler_config_provider=config,
