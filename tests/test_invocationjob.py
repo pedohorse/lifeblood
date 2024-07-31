7f288d272fa47c88dc05719fff642ebddb4cc871
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-07-31 18:16:38 +0200

    environment unit tests

--- tests/test_invocationjob.py
+++ tests/test_invocationjob.py
@@ -1,8 +1,116 @@
+import os
 from unittest import TestCase
 import random
 from string import ascii_letters
 from lifeblood.invocationjob import (Requirements, ResourceRequirements, ResourceRequirement, DeviceRequirements,
-                                     DeviceRequirement, Invocation, InvocationJob, InvocationResources)
+                                     DeviceRequirement, Invocation, InvocationJob, InvocationResources, Environment,
+                                     InvocationEnvironment)
+
+
+class TestEnvironment(TestCase):
+    def test_init_set_append_prepend(self):
+        # init
+        env0 = Environment()
+        self.assertDictEqual({}, env0)
+        env1 = Environment({
+            'initi': 234,
+            'initf': -42.11,
+            'inits': "vaaaaa",
+            'initincomp': None,
+        })
+        self.assertEqual('234', env1['initi'])
+        self.assertEqual('-42.11', env1['initf'])
+        self.assertEqual('vaaaaa', env1['inits'])
+        self.assertEqual('None', env1['initincomp'])
+
+        # set
+        env1['fee'] = 135
+        self.assertEqual('135', env1['fee'])
+        env1['fee'] = 'goora'
+        self.assertEqual('goora', env1['fee'])
+
+        # append/prepend
+        env1.append('fee', 'booba')
+        self.assertEqual(os.pathsep.join(('goora', 'booba')), env1['fee'])
+        env1.prepend('fee', 'zooga')
+        self.assertEqual(os.pathsep.join(('zooga', 'goora', 'booba')), env1['fee'])
+        env1.append('bar1', 'eerrtt')
+        self.assertEqual('eerrtt', env1['bar1'])
+        env1.append('bar2', 'zzxxcc')
+        self.assertEqual('zzxxcc', env1['bar2'])
+
+        # all
+        self.assertDictEqual({
+            'initi': '234',
+            'initf': '-42.11',
+            'inits': "vaaaaa",
+            'initincomp': 'None',
+            'fee': os.pathsep.join(('zooga', 'goora', 'booba')),
+            'bar1': 'eerrtt',
+            'bar2': 'zzxxcc',
+        }, env1)
+
+    def test_expand_variables(self):
+        env = Environment({'foo': 'bar', 'hur': 'bla'})
+        env.append('qwe', 'ba$foo')
+        env.prepend('rty', 'we${hur}t')
+        self.assertEqual('babar', env['qwe'])
+        self.assertEqual('weblat', env['rty'])
+        env['foo'] = '$foo'
+        self.assertEqual('bar', env['foo'])
+        env['foo'] = '!!${foo}@@'
+        self.assertEqual('!!bar@@', env['foo'])
+        env['goo'] = 'no $none'
+        self.assertEqual('no ', env['goo'])
+
+
+class TestInvocationEnvironment(TestCase):
+    def test_resolve(self):
+        env1 = InvocationEnvironment()
+        self.assertDictEqual({}, env1.resolve())
+
+        env1['fee'] = 'qet'
+        env1.append('fee', 'poop')
+        env1.prepend('fee', 'gloop')
+
+        env1['ber'] = 'qq$fee'
+
+        self.assertDictEqual({
+            'fee': os.pathsep.join(('gloop', 'qet', 'poop')),
+            'ber': 'qq' + os.pathsep.join(('gloop', 'qet', 'poop')),
+        }, env1.resolve())
+
+    def test_extend(self):
+        env1 = InvocationEnvironment()
+        env2 = InvocationEnvironment()
+
+        env1['qwe'] = 'rty'
+        env1.prepend('asd', 'fgh')
+
+        env2.append('qwe', 'uio')
+        env2['asd'] = 'fgh'
+
+        env1.extend(env2)
+
+        self.assertDictEqual({
+            'qwe': os.pathsep.join(('rty', 'uio')),
+            'asd': 'fgh',
+        }, env1.resolve())
+
+    def test_base_env(self):
+        env1 = InvocationEnvironment()
+        env1['asd'] = 'fgh'
+        env1['zxc'] = 'bob $foo'
+
+        self.assertDictEqual({
+            'qwe': 'rty',
+            'foo': 'bar',
+            'asd': 'fgh',
+            'zxc': 'bob bar'
+        }, env1.resolve(Environment({
+            'qwe': 'rty',
+            'foo': 'bar',
+        })))
 
 
 class TestRequirements(TestCase):
