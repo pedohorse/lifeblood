b208d5e5f5c267632f57a21263052bec216b54cc
pedohorse <13556996+pedohorse@users.noreply.github.com>
2025-02-04 00:05:26 +0100

    add existance check before rm since we now fail on fail...

--- dcc_plugins_workspace/build_houdini.sh
+++ dcc_plugins_workspace/build_houdini.sh
@@ -22,7 +22,9 @@ rsync -arhv --exclude=__pycache__ --exclude="*.pyc" ../src/lifeblood_client $OUT
 rsync -arhv --exclude=__pycache__ --exclude="*.pyc" ../src/lifeblood_client $OUT/python3.11libs
 
 pushd $OUT
-rm ../houdini.zip
+if [ -f ../houdini.zip ]; then
+  rm ../houdini.zip
+fi
 zip -r ../houdini.zip . --exclude "*__pycache__/" --exclude "*.pyc" --exclude "*.md"
 popd
 
