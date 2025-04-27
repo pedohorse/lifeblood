22f8e5c6ed133ff3e5bb90cdbdf674508a531ec6
pedohorse <13556996+pedohorse@users.noreply.github.com>
2025-04-27 22:56:04 +0200

    include certifi

--- stock_tools/telegram_client/compile.sh
+++ stock_tools/telegram_client/compile.sh
@@ -5,11 +5,19 @@ if [ -d urllib3 ]; then
   echo "cleanup"
   rm -rf urllib3
 fi
+if [ -d certifi ]; then
+  echo "cleanup"
+  rm -rf certifi
+fi
 pip download "urllib3>=2,<3"
+pip download "certifi"
 unzip urllib3*.whl
 rm urllib3*.whl
+unzip certifi*.whl
+rm certifi*.whl
 rm -rf urllib3*.dist-info
-zip -r telegram_client.pyz __main__.py telebot.py urllib3/
+rm -rf certifi*.dist-info
+zip -r telegram_client.pyz __main__.py telebot.py urllib3/ certifi/
 mv telegram_client.pyz ../../src/lifeblood/stock_nodes/telegram_client/data/.
 printf "all done\n"
 
