f9d4db11d2b4a1e462e76918fd163ee35f2dddb8
pedohorse <13556996+pedohorse@users.noreply.github.com>
2025-06-10 11:15:23 +0200

    minor comment added

--- src/lifeblood/net_messages/client.py
+++ src/lifeblood/net_messages/client.py
@@ -50,7 +50,7 @@ class MessageClient:
             try:
                 try:
                     stream = await self.__message_stream_factory.open_sending_stream(self.__destination[0], self.__source[0])
-                except OSError as e:
+                except OSError as e:  # this is supposed to catch ALL network-related errors only
                     raise MessageSendingError(wrapped_exception=e) from None
                 message = await stream.send_data_message(data, self.__destination_str, source=self.__source_str, session=self.__session)
                 self.__last_sent_message = message
