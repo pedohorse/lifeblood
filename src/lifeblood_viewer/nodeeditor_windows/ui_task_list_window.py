db3d01f1237f1960e6e2911fbbbfa4e1be40e79d
pedohorse <13556996+pedohorse@users.noreply.github.com>
2025-06-15 18:10:40 +0200

    add frames attr sanity check

--- src/lifeblood_viewer/nodeeditor_windows/ui_task_list_window.py
+++ src/lifeblood_viewer/nodeeditor_windows/ui_task_list_window.py
@@ -71,7 +71,6 @@ class TaskListWindow(ImguiViewWindow, NetworkItemWatcher):
                     sort_spec = imgui.table_get_sort_specs()
                     sort_order = self.__displayed_node.TaskSortOrder.ID
                     if sort_spec.specs_count:
-                        print(sort_spec.specs[0].column_index, sort_spec.specs[0].column_user_id, sort_spec.specs[0].sort_direction, sort_spec.specs[0].sort_order)
                         spec = sort_spec.specs[0]
                         if spec.column_index == 0:
                             if spec.sort_direction == imgui.SORT_DIRECTION_ASCENDING:
@@ -123,7 +122,7 @@ class TaskListWindow(ImguiViewWindow, NetworkItemWatcher):
 
                         imgui.table_next_column()
 
-                        if frames := task.attributes().get('frames'):
+                        if (frames := task.attributes().get('frames')) and isinstance(frames, list):
                             if len(frames) == 1:
                                 imgui.text(str(frames[0]))
                             else:
