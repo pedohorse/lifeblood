c7969ef24d8f0233b5d7c3559c8597266056d7a9
pedohorse <13556996+pedohorse@users.noreply.github.com>
2025-08-22 17:34:07 +0200

    add clear_internal_state interface

--- src/lifeblood/net_messages/message_handler.py
+++ src/lifeblood/net_messages/message_handler.py
@@ -9,3 +9,12 @@ class MessageHandlerBase:
         if message is not expected by this handler - the function should return False
         """
         raise NotImplementedError()
+
+    async def clear_internal_state(self):
+        """
+        If a message handler implementation has an internal state -
+         this function must clean it.
+        This might be called when handler is de-initialized,
+        or when some other clear slate reset is required
+        """
+        return
